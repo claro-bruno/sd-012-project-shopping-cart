bae7c1a66c285f378c21a96fd3b362682f6a194e
Gabriel Takaki <gabitj@icloud.com>
2021-06-09 18:18:08 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+// const fetch = require('node-fetch');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,30 +14,56 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// async function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const sectionItems = document.querySelector('.items');
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+const verifyFetch = async () => {
+  const fetchingApi = fetch(API_URL)
+    .then((response) => response
+    .json()
+      .then((data) => data));
+  return fetchingApi;    
+};
+
+const returningObj = async () => {
+  try {
+    const obj = await verifyFetch();
+    const objResolve = obj.results;
+
+    objResolve.forEach((element) => sectionItems.appendChild(createProductItemElement(element)));
+  } catch (error) {
+    console.log('Error');
+  }
+};
+
+window.onload = function onload() {
+  returningObj();
+};
