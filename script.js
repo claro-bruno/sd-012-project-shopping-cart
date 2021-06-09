f63b82c5041fbdc7f084fa6a78243299b86c8506
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-09 20:15:39 -0300

    (5) - Adiciona funçao de limpar carrinho

--- script.js
+++ script.js
@@ -66,6 +66,11 @@ const appendCartItem = (info) => {
   cart.appendChild(data);
 };
 
+const updatePrice = ({ price }) => {
+  const price2 = document.querySelector('.total-price');
+  price2.innerHTML = parseFloat(price2.innerHTML) + price;
+};
+
 document.addEventListener('click', async (event) => {
   if (event.target.className === 'item__add') {
     const section = event.target.parentElement;
@@ -73,12 +78,28 @@ document.addEventListener('click', async (event) => {
     const info = await fetch(`https://api.mercadolibre.com/items/${itemSku}`)
     .then((response) => response.json());
     appendCartItem(info);
+    updatePrice(info);
     localStorage.setItem('cartList', document.querySelector(cartItems).innerHTML);
   }
+});
+
+const totalPrice = () => {
+  const cart = document.querySelector('.cart');
+  const total = createCustomElement('span', 'total', 'Total: $ ');
+  const price = createCustomElement('span', 'total-price', 0);
+  cart.appendChild(total);
+  total.appendChild(price);
+};
+
+document.querySelector('.empty-cart').addEventListener('click', () => {
+  document.querySelector(cartItems).innerHTML = '';
+  document.querySelector('.total-price').innerHTML = 0;
+  localStorage.setItem('cartList', document.querySelector(cartItems).innerHTML);
 });
 
 window.onload = function onload() {
   const cartList = document.querySelector(cartItems);
   cartList.innerHTML = localStorage.getItem('cartList');
   appendComputers();
+  totalPrice();
 };
