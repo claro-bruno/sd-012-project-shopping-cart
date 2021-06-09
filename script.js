88a87e469d1494438c6a0f0789abf6629de364fc
Teika <shimworks.tk@gmail.com>
2021-06-09 17:21:28 -0300

    resolve requisitos 1 a 3

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +24,41 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+function cartItemClickListener({ target }) {
+  target.remove();
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: SalePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${SalePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const sec = document.getElementsByClassName('items');
+const fat = async () => {
+  let objAPI = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+  objAPI = await objAPI.json();
+  objAPI.results.forEach((API) => sec[0].appendChild(createProductItemElement(API)));
+};
+
+const ol = document.getElementsByClassName('cart__items');
+async function addItemToCart(event) {
+  let itemAPI = await fetch(`https://api.mercadolibre.com/items/${event}`);
+  itemAPI = await itemAPI.json();
+  ol[0].appendChild(createCartItemElement(itemAPI));
+}
+
+document.addEventListener('click', function ({ target }) {
+  if (target.classList.contains('item__add')) {
+    const itemID = target.parentElement.firstChild.innerText;
+    addItemToCart(itemID);
+  }
+});
+
+window.onload = function onload() { fat(); };
