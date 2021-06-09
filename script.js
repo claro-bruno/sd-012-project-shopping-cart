affa7378c43f9bb4c6a6435444390b8d9f5fe32c
alestori <alestori7@gmail.com>
2021-06-09 16:43:17 -0300

    requisito 2 completo

--- script.js
+++ script.js
@@ -9,9 +9,27 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 const addButton = (e) => {
   const addedId = getSkuFromProductItem(e.target.parentElement);
-  console.log(addedId);
+  const cart = document.querySelector('.cart__items');
+  return new Promise((pass, fail) => {
+    fetch(`https://api.mercadolibre.com/items/${addedId}`)
+      .then((result) => result.json())
+      .then((json) => pass(cart.appendChild(createCartItemElement(json))))
+      .catch((error) => fail(error));
+  });
 };
 
 function createCustomElement(element, className, innerText) {
@@ -33,18 +51,6 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 // ajuda de matheus set, da turma 11
@@ -69,11 +75,11 @@ const loadProducts = (query) => {
   toggleLoadingText(true);
   return new Promise((pass, fail) => {
     fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
-    .then((result) => result.json())
-    .then((json) => {
-      toggleLoadingText(false);
-      pass(json.results);
-    })
+      .then((result) => result.json())
+      .then((json) => {
+        toggleLoadingText(false);
+        pass(json.results);
+      })
     .catch((error) => fail(error));
   });
 };
