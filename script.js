4935ad5e462cb12b6c1d88873998af8be5376464
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-06-09 15:54:17 -0300

    Requisito 1 - Resolvido com Lint

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,17 +12,29 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+const items = document.getElementsByClassName('items');
+
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
+async function pegaAPI() {
+  let resposta = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  resposta = await resposta.json();
+  resposta = await resposta.results;
+  resposta.forEach((computador) => { 
+    items[0].appendChild(createProductItemElement(computador));
+  });
+}
+
+pegaAPI();
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -41,3 +51,5 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() { pegaAPI(); };
