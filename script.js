af1b9a3bc4426662c3956cbcb24a6f12b2d207ac
Adryan Rosa <adryanrosa374@gmail.com>
2021-06-09 17:02:41 -0300

    Correção requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // window.onload = function onload() { };
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -15,45 +15,31 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement("section");
-  section.className = "item";
+  const section = document.createElement('section');
+  section.className = 'item';
 
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(
-    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
-  );
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
 
-const getItems = () =>
-  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador").then(
-    (response) =>
-      response.json().then((computers) => {
-        computers.results.forEach((computer) => {
-          const computerFunc = computer;
-          computerFunc.sku = computerFunc.id;
-          computerFunc.name = computerFunc.title;
-          computerFunc.image = computerFunc.thumbnail;
-          const items = document.getElementsByClassName("items")[0];
-          const item = createProductItemElement(computerFunc);
-          items.appendChild(item);
-          item.addEventListener("click", cartItemClickListener);
-        });
-      })
-  );
-
-window.onload = function onload() {
-  getItems();
-};
-
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
-const getItem = (id) =>
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartEvent);
+  return li;
+}
+
+function cartItemClickListener(target) {
+  const id = getSkuFromProductItem(target.path[1]);
   fetch(`https://api.mercadolibre.com/items/${id}`).then((response) =>
     response.json().then((computer) => {
       const computerFunc = computer;
@@ -63,18 +49,24 @@ const getItem = (id) =>
       const items = document.getElementsByClassName('cart__items')[0];
       const item = createCartItemElement(computerFunc);
       items.appendChild(item);
-    })
-  );
-
-function cartItemClickListener(target) {
-  const id = getSkuFromProductItem(target.path[1]);
-  getItem(id);
+    }));
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+const getItems = () => fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json()
+    .then((computers) => {
+      computers.results.forEach((computer) => {
+        const computerFunc = computer;
+        computerFunc.sku = computerFunc.id;
+        computerFunc.name = computerFunc.title;
+        computerFunc.image = computerFunc.thumbnail;
+        const items = document.getElementsByClassName('items')[0];
+        const item = createProductItemElement(computerFunc);
+        items.appendChild(item);
+        item.addEventListener('click', cartItemClickListener);
+      });
+    }));
+
+window.onload = function onload() {
+  getItems();
+};
