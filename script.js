01468367d8471702208f2100f72c72802ecbae22
André Moreno <deco616@hotmail.com>
2021-06-09 20:40:24 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -28,19 +28,19 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-const fetchApi = () => {
+const fetchApi = async () => {
   const element = document.querySelector('.items');
 
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
@@ -51,12 +51,28 @@ const fetchApi = () => {
       const objeto = {
         sku: item.id,
         name: item.title,
-        salePrice: item.price,
+        image: item.thumbnail,
       };
       element.appendChild(createProductItemElement(objeto));
     });
+  }); 
+};
+
+const handleButton = (id) => {
+  const cart = document.querySelector('.cart__items');
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((response) => response.json())
+  .then(({ id: sku, title: name, price: salePrice }) => {
+    cart.appendChild(createCartItemElement({ sku, name, salePrice }));
   });
 };
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('item__add')) {
+    const id = event.target.parentNode.firstChild.innerText;
+    handleButton(id);
+  }
+});
 
 window.onload = function onload() {
   fetchApi();
