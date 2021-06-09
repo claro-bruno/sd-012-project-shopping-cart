d963937c282ed4478162d404355bd15aa3911aae
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-09 17:01:08 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -30,25 +30,42 @@ function createProductItemElement({
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function getButton(item) {
+  return item.querySelector('button.item__add');
+}
 
-// function createCartItemElement({
-//   sku,
-//   name,
-//   salePrice,
-// }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function cartItemClickListener(event) {
+  console.log(event);
+  const cart = document.querySelector('.cart__items');
+  cart.appendChild(event);
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const activateButtons = () => {
+  const allSectionsProducts = document.querySelectorAll('.item');
+  allSectionsProducts.forEach((item) => {
+    const id = getSkuFromProductItem(item);
+    const button = getButton(item);
+    const urlItem = `https://api.mercadolibre.com/items/${id}`;
+    button.addEventListener('click', () => {
+      fetch(urlItem).then((data) => data.json()).then((file) => {
+        const { id: sku, title: name, price: salePrice } = file;
+        cartItemClickListener(createCartItemElement({ sku, name, salePrice }));
+      });
+    });
+  });
+};
 
 window.onload = async function computers() {
   const link = await fetch(url);
@@ -56,16 +73,9 @@ window.onload = async function computers() {
   const resultados = await obj.results;
   resultados.forEach((product) => {
     const allItemsList = document.querySelector('.items');
-    const {
-      id: sku,
-      title: name,
-      thumbnail: image,
-    } = product;
-    const newItem = createProductItemElement({
-      sku,
-      name,
-      image,
-    });
+    const { id: sku, title: name, thumbnail: image } = product;
+    const newItem = createProductItemElement({ sku, name, image });
     allItemsList.appendChild(newItem);
   });
+  activateButtons();
 };
