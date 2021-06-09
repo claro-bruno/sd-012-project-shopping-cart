9384bd341f6560457c33124f3611cefa58aece65
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-06-09 20:55:11 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function cartItemClickListener(event) {
+    // coloque seu código aqui
+    return event;
+}
+
+function createCartItemElement({ id, title, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const addToCart = async (event) => {
+  const id = event.target.parentElement.firstChild.innerHTML;
+  let searchAPI = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  searchAPI = await searchAPI.json();
+  const olCart = document.querySelector('.cart__items');
+  olCart.appendChild(createCartItemElement(searchAPI));
+};
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,10 +26,11 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText, functiones) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  e.addEventListener('click', functiones);
   return e;
 }
 
@@ -20,7 +42,7 @@ function createProductItemElement({ id, title, thumbnail }) {
   section.appendChild(createCustomElement('span', 'item__title', title));
   section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button',
-   'item__add', 'Adicionar ao carrinho!'));
+  'item__add', 'Adicionar ao carrinho!', addToCart));
   return section;
 }
 
@@ -34,21 +56,9 @@ const fetchItems = async () => {
 };
 
 // function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ id, title, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+  //   return item.querySelector('span.item__sku').innerText;
+  // }
 
 window.onload = function onload() {
   fetchItems();
- };
+};
