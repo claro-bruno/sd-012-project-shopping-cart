e7d5f2df93dd73a9a2902de8d0374d58a4fb3b1f
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-06-09 18:40:21 -0300

    Inicia resolucao da funcao createProductItemElement

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function createProductImageElement(imageSource) {
+/* eslint-disable max-lines-per-function */
+/* function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+} */
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
@@ -12,19 +13,23 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
+  console.log(sku, name, image);
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  // section.appendChild(createProductImageElement(image));
+  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  return section;
+  const sectionItems = document.querySelector('.items');
+  sectionItems.appendChild(section);
+
+  // return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
@@ -38,19 +43,20 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
 
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-const fetchML = () => {
+const fetchAPI = () => {
   const myObject = {
     method: 'GET',
     headers: { Accept: 'application/json' },
   };
   fetch(API_URL, myObject)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((response) => response.results)
+    .then((response) => response.forEach((item) => createProductItemElement(item)));    
 };
 
 window.onload = function onload() { 
-  fetchML();
+  fetchAPI();
 };
