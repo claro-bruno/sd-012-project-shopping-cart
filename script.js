e8b811e5db8259de9c01bc71a4c84e4cd13b1796
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-06-09 19:35:52 -0300

    Resolvendo requisito 3

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  const lista = document.querySelector('.cart__items');
+  lista.removeChild(event.target);
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -48,10 +53,6 @@ function createProductItemElement({ sku, name, image }) {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 async function verifetch(url) {
