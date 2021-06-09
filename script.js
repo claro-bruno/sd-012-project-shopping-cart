b1118d65397c7c3f1b5bf1d5b884a8891ad95508
Roberval Filho <rslfilho@gmail.com>
2021-06-09 16:03:17 -0300

    Iniciando projeto com requisito 01

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +24,50 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const appendItem = (element, parentClass) => {
+  const parentElement = document.querySelector(parentClass);
+
+  parentElement.appendChild(element);
+};
+
+const fetchItemList = async () => {
+  const productURL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+  const fetchObject = fetch(productURL)
+    .then((response) => response
+      .json()
+      .then((object) => object));
+  
+  return fetchObject;
+};
+
+const createItemList = async () => {
+  try {
+    const fetchedObject = await fetchItemList();
+    const productList = fetchedObject.results;
+    // console.log(productList);
+    productList.forEach((item) => appendItem(createProductItemElement(item), '.items'));
+  } catch (error) {
+    console.log('Error');
+  }
+};
+
+window.onload = function onload() {
+  createItemList();
+};
