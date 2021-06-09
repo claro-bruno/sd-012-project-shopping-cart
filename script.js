f53e95d3b4bb1166a7c277f1c0a7c48f9a6c3298
Raphael <raphael.ph2ll@gmail.com>
2021-06-09 19:38:06 -0300

    Resolve ex01 - listagemDeProdutos

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -8,16 +6,20 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
+  // Cria elemento
   const e = document.createElement(element);
+  // Adiciona uma classe e um texto 
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-
-function createProductItemElement({ sku, name, image }) {
+// 'Reatribuindo os valores que o parametro vai receber'
+function createProductItemElement({ id: sku, tittle: name, thumbnail: image }) {
+  // Cria elemento
   const section = document.createElement('section');
+  // Da uma classe a ele
   section.className = 'item';
-
+  // Adiciona 'filhos'
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -26,18 +28,32 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+// Requisito 1 onload fetch API
+const items = document.querySelector('.items');
+const fetchMercadoLivre = () => {
+  const ApiMercadoLivre = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(ApiMercadoLivre)
+  .then((response) => response.json())
+  .then((arr) => arr.results.forEach((elements) => items
+  .appendChild(createProductItemElement(elements))));
+};
+
+window.onload = function onload() {
+  fetchMercadoLivre();
+};
