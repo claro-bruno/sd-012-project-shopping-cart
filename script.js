56e5d1af01a62d8e7fa18fd155dceb4bc90cea99
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-06-09 15:59:48 -0300

    Requisito 1 | fetchProductList()

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) { // id, title, thumbnail
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,23 +20,38 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const itemsSection = document.querySelector('.items');
+  itemsSection.appendChild(section);
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-  //inicia projeto
-}
+  // inicia projeto
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+} */
+
+async function fetchProductList(product) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  const { results } = await response.json();
+  console.log(results);
+  results.forEach((element) => {
+    createProductItemElement(element);
+  });
 }
+
+window.onload = function onload() { 
+  fetchProductList('computador');
+};
