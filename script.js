84caed03e8b1df517d7e1f7b1e9be2ac996967d1
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-06-09 17:20:47 -0300

    inicia projeto

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-window.onload = function onload() { };
+window.onload = function onload() {};
+
+
+      
+        
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
