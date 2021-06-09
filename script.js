617dfa38844808c3112d8e66ec0b4c43da4fea2a
Wadson <wadson.dev92@gmail.com>
2021-06-09 20:56:30 -0300

    requisito 1, testando linter

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-window.onload = function onload() { };
+window.onload = async () => {
+  try {
+
+  const item = await pegarItem();
+  item.results.forEach((itemML) => createProductItemElement(itemML))
+
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+function pegarItem () {
+  return new Promise((resolve, reject) => {
+
+    fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+    .then((response) => response.json())
+    .then((array) => resolve(array))
+    .catch((error) => reject(error))
+  });
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,16 +33,19 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
   const section = document.createElement('section');
   section.className = 'item';
+  const product = document.querySelector('.items')
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  return section;
+  product.appendChild(section)
+  // return section;
+
 }
 
 function getSkuFromProductItem(item) {
