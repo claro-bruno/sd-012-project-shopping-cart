cb9440859871f0d0d6e969e095aa515b0888229d
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-06-09 18:23:19 -0300

    Bora encher o carrinho

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  
+ };
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
