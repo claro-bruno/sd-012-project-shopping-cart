b280e5d4a8fb94615dba5217c94ae2e62b28455b
Daniel <danielbped@gmail.com>
2021-06-09 18:39:42 -0300

    Refatora requisito 1

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const buttons = document.querySelectorAll('.item__add');
 
-function createProductImageElement(imageSource) {
+const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
-function createCustomElement(element, className, innerText) {
+const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,36 +25,34 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
+};
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const cartItemClickListener = (event) => {};
 
-function createCartItemElement({ sku, name, salePrice }) {
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+};
 
-async function fetchApi(url) {
+const createItemsList = (data) => {
   const items = document.querySelector('.items');
-      await fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          const itemsList = data.results;
-          itemsList.forEach((item) => {
-            const itemElement = createProductItemElement(item);
-            items.appendChild(itemElement);
-          });
-        });
-}
+  const itemsList = data.results;
+    itemsList.forEach((item) => {
+      const itemElement = createProductItemElement(item);
+      items.appendChild(itemElement);
+    });
+};
+
+const fetchApi = async (url) => {
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => createItemsList(data));
+};
 
 window.onload = function onload() {
   fetchApi(API_URL);
