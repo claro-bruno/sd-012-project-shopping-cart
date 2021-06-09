996ee244ccd7846288a0747e2fb7cc1787cff748
thales <thalesdaiel2013@gmail.com>
2021-06-09 20:18:53 -0300

    primeiro commit, VQV.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+ teste
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
