f4e809989b1244ea40f15cdb8ee08a3508f9c202
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-09 19:03:47 -0300

    entrega requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let cartData = [];
-// const cartItemsClass = '.cart__items';
+let shoppingCart = [];
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -30,24 +29,15 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-// REQUISITO 4.1
+// REQUISITO 4.1 - Salva os itens do carrinho no Local Storage
 function localStorageCart() {
-  localStorage.setItem('cart-data', JSON.stringify(cartData));
+  localStorage.setItem('shopping-data', JSON.stringify(shoppingCart));
 }
-// REQUISITO 3
+// REQUISITO 3 - Remove do carrinho os itens clicados
 function cartItemClickListener(event) {
   event.target.remove();
   localStorageCart();
 }
-
-// REQUISITO 5
-// function payment() {
-// const payout = document.querySelector('.total-price');
-// const list = [...document.querySelectorAll('.cart__item')];
-
-//   const sum = list.reduce((acc, value) => acc + Number(value.innerText.split('PRICE: $')[1]), 0);
-//   payout.innerText = sum;
-// }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -57,35 +47,35 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+// REQUISITO 2.2 - Adiciona o item clicado no carrinho
 function insertCartItem() {
   const cartItem = document.querySelector('.cart__items');
   cartItem.innerHTML = '';
-  cartData.forEach((item) => {
+  shoppingCart.forEach((item) => {
     const cartItemElement = createCartItemElement(item);
     cartItem.appendChild(cartItemElement);
   });
 }
-// REQUISITO 2
+// REQUISITO 2.1 - Busca os detalhes do produto clicado para adicionar no carrinho
 async function addToCart(event) {
   const sku = getSkuFromProductItem(event.target.parentElement);
   const response = await fetch(`https://api.mercadolibre.com/items/${sku}`);
   const { title, price } = await response.json();
-  cartData.push({ sku, name: title, salePrice: price });
+  shoppingCart.push({ sku, name: title, salePrice: price });
   insertCartItem();
   localStorageCart();
 }
-// REQUISITO 4.2
+// REQUISITO 4.2 - Resgata os itens do carrinho salvo no Local Storage
 function retrieveLocalStorage() {
-  const dataToRetrieve = JSON.parse(localStorage.getItem('cart-data'));
+  const dataToRetrieve = JSON.parse(localStorage.getItem('shopping-data'));
   if (dataToRetrieve) {
-    cartData = [...dataToRetrieve];
+    shoppingCart = [...dataToRetrieve];
     insertCartItem();
   }
 }
-// REQUISITO 1
+// REQUISITO 1 - Insere a lista de produtos na página
 async function productList() {
   const itemSection = document.querySelector('section .items');
-  
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const { results } = await response.json();
   results.forEach((product) => {
@@ -98,9 +88,18 @@ async function productList() {
     itemSection.appendChild(component);
   });
 }
+// REQUISITO 6 - Apaga os itens do carrinho ao clicar no botão
+function clearCart() {
+  const clearButton = document.querySelector('.empty-cart');
+  clearButton.addEventListener('click', () => {
+    document.querySelector('.cart__items').innerHTML = ' ';
+    document.querySelector('.total-price').innerHTML = 0;
+    localStorage.clear();
+  });
+}
 
 window.onload = async function onload() {
   await productList();
-  // localStorageCart();
   retrieveLocalStorage();
+  clearCart();
 };
