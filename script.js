bca3da6bf0f9dd11c9a274229d580a5a7753e91d
Adryan Rosa <adryanrosa374@gmail.com>
2021-06-09 17:16:51 -0300

    Satisfaz requisito 3

--- script.js
+++ script.js
@@ -30,15 +30,19 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartEvent);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-function cartItemClickListener(target) {
+function addItemToCart(target) {
   const id = getSkuFromProductItem(target.path[1]);
   fetch(`https://api.mercadolibre.com/items/${id}`).then((response) =>
     response.json().then((computer) => {
@@ -63,7 +67,7 @@ const getItems = () => fetch('https://api.mercadolibre.com/sites/MLB/search?q=co
         const items = document.getElementsByClassName('items')[0];
         const item = createProductItemElement(computerFunc);
         items.appendChild(item);
-        item.addEventListener('click', cartItemClickListener);
+        item.addEventListener('click', addItemToCart);
       });
     }));
 
