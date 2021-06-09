de9e7cef73266a5514ea98adce474ce55c33c008
igor <igorsife80@gmail.com>
2021-06-09 14:51:15 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function onload() { };
 
+const xablau = 'mano xabs'
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
