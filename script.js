4cc5e84f6b6218d268f2c72f6cb268138a269fe0
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-06-09 15:38:14 -0300

    Adiciona requisito 1

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-window.onload = function onload() {
-
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -16,13 +12,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
@@ -36,10 +32,43 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// Gera a Promise do Computador
+
+const getComputerPromise = (computerName) => new Promise((resolve, reject) => {
+  if (computerName !== 'computador') {
+    reject(new Error('Nome Errado'));
+  } else {
+    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computerName}`)
+    .then((response) => {
+      response.json().then((computer) => {
+        const section = createProductItemElement(computer.results);
+        const sectionPai = document.querySelector('.items');
+        computer.results.map((computerUnit) => sectionPai
+          .appendChild(createProductItemElement(computerUnit)));        
+        resolve();
+      });
+    });
+  }
+});
+
+// fetch Computer
+
+const fetchComputer = async () => {
+  try {
+    await getComputerPromise('computador');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.onload = function onload() {
+  fetchComputer();
+};
