1fd641be95e35dd6efa3e05312204ff42b68543f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-09 19:17:12 -0300

    requisito 3 - Remova o item do carrinho de compras ao clicar nele

--- script.js
+++ script.js
@@ -30,15 +30,15 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.parentElement.removeChild(event.target);
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
