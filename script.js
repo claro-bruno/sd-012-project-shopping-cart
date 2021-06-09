c2f40a5b0e5c724a06b3ecb9768832697efb01f4
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-06-09 17:26:38 -0300

    adiciona req 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,10 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -41,3 +39,18 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function getItem() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((r) => r.json())
+    .then((json) => {
+      json.results.forEach((result) => {
+        const itemsSec = document.querySelector('#items');
+        itemsSec.appendChild(createProductItemElement(result));
+      }); 
+    });  
+}
+
+window.onload = function onload() { 
+  getItem();
+};
