131a75473762f639b8f076289a8893f1e1e5fc31
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-06-09 15:43:15 -0300

    primeiras alteraçoes no projeto, enviando para conferencia

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+const cartItems = document.querySelector('.cart__items');
+const cart = document.querySelector('.cart');
+const allitemsPrice = document.querySelector('.total-price');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +16,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -30,8 +32,9 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+function cartItemClickListener(event, salePrice) {
+  cartItems.removeChild(event.target);
+  allitemsPrice.innerText = parseFloat(Number(allitemsPrice.innerText) - salePrice);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -41,3 +44,46 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function createProductList() {
+  const itemsList = document.querySelector('.items');
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((json) => {
+    json.results.forEach((result) => {
+      itemsList.appendChild(createProductItemElement(result));
+    });
+  })
+  .then(() => {
+    cart.removeChild(document.querySelector('.loading'));
+    for (let index = 0; index < localStorage.length; index += 1) {
+      const [id, title, salePrice] = localStorage.getItem(`Produto${index}`).split('|');
+      const result2 = { id, title, salePrice };
+      cart.appendChild(createCartItemElement(result2));
+    }
+  });
+}
+
+function addProduct(e) {
+  if (e.target.className === 'item__add') {
+    const id = getSkuFromProductItem(e.target.parentNode);
+    fetch(`https://applicationCache.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((json) => {
+      const itemList = document.querySelector('.cart__items');
+      itemList.appendChild(createCartItemElement(json));
+    });
+  }
+}
+
+window.onload = function onload() {
+  createProductList();
+  const items = document.querySelector('.items');
+  items.addEventListener('click', addProduct);
+  const button = document.querySelector('.empty-cart');
+  button.addEventListener('click', () => {
+    cartItems.innerHTML = '';
+    allitemsPrice.innerText = '0';
+    localStorage.clear();
+  });
+};
