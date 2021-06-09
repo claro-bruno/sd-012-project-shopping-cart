f8028a18d368823c1bb635254dc71eb1e91c0ada
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-09 18:25:20 -0300

    Resolve o requisito 2

--- script.js
+++ script.js
@@ -12,23 +12,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-// prettier-ignore
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -39,6 +22,38 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function addCart(sku) {
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then((item) => item.json())
+    .then((product) => {
+      const obj = { sku: '', name: '', salePrice: '' };
+      obj.sku = product.id;
+      obj.name = product.title;
+      obj.salePrice = product.price;
+      const cartItem = createCartItemElement(obj);
+      document.querySelector('.cart__items').appendChild(cartItem);
+    });
+}
+
+// prettier-ignore
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.appendChild(button);
+  button.addEventListener('click', (t) => addCart(t.target.parentNode.firstChild.innerText));
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 // prettier-ignore
@@ -56,7 +71,7 @@ function loadProducts() {
           .querySelector('.items')
           .appendChild(createProductItemElement(obj));
       }))
-    .then((r) => document.querySelector('.loading').remove())
+    .then(() => document.querySelector('.loading').remove())
     .catch((e) => console.log(e));
 }
 
