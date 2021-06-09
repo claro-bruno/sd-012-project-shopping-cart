accdaee386fbf2db6079e2be04b5bec25caac63e
Eric Kreis <erickreis793@gmail.com>
2021-06-09 14:34:54 -0300

    Iniciando projeto VQV! :rocket:

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
