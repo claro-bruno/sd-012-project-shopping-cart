067601e45cc7614c82184740c4a16454d58be50e
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-06-09 17:25:49 -0300

    inicio do projeto

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 window.onload = function onload() {};
 
-
-      
-        
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
