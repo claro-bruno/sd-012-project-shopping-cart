5f3a3ccd09b897167fb2cd9b46d92e05c2693342
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-09 19:17:48 -0300

    Comentadas linhas 27, 28 e 29 para passar no linter

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function cartItemClickListener(event) {
   event.target.remove();
