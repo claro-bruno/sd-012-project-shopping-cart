900866dd0cbe8ef87a514c9b66e6b08b9aab3b48
Dayvid Ruan <ruan12bbg@gmail.com>
2021-06-10 18:46:47 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  carr.removeChild(event.target);
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
