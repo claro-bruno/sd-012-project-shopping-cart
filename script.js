606992a2eb9d607e495ebf00631ec5dc63cf45d4
Wadson <wadson.dev92@gmail.com>
2021-06-10 00:32:56 -0300

    tentando add item no carrinho, sem sucesso :(

--- script.js
+++ script.js
@@ -39,17 +39,35 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  const botao = document.querySelectorAll('.item__add');
+  botao.forEach((btn) => {
+  btn.addEventListener('click', async (event) => {
+    const xablau = event.target.className.querySelector('cart__item')
+    const itemSelected = await addItem(xablau);
+    createCartItemElement(itemSelected)
+    console.log('oiii')
+    // const cartItem = document.querySelector('cart__items')
+  })
+})
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function addItem() {
+  return new Promise((resolve, reject) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=id')
+    .then((response) => response.json())
+    .then((array) => resolve(array))
+    .catch((error) => reject(error));
+  });
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 window.onload = async () => {
   try {
