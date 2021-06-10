ac95477201800e1cd812b1a62d8e86db997a4846
RyanLaiber <ryanlaiber@gmail.com>
2021-06-09 22:13:50 -0300

    continuo amanha

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const totalPrice = () => {
+  const cart = document.getElementsByClassName('cart')[0];
+  const priceElement = document.createElement('section');
+  const textElement = document.createElement('label'); 
+  priceElement.className = 'total-price';
+  textElement.className = 'price-label';
+  cart.appendChild(textElement);
+  cart.appendChild(priceElement);
+  priceElement.innerText = '0';
+  textElement.innerText = 'Total Price:';
+};
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,14 +19,26 @@ function createProductImageElement(imageSource) {
 
 const updateStorage = () => {
   const cartItems = document.querySelectorAll('.cart__items')[0];
+  const totalPriceElement = document.getElementsByClassName('total-price')[0];
   if (localStorage.carrinho) {
     localStorage.carrinho = cartItems.innerHTML;
   } else {
     localStorage.setItem('carrinho', cartItems.innerHTML);
   }
+  if (localStorage.totalPrice) {
+    localStorage.totalPrice = totalPriceElement.innerHTML;
+  } else {
+    localStorage.setItem('totalPrice', 0);
+  }
 };
 
 function cartItemClickListener(event) {
+  const innerText = event.target.innerText;
+  const price = parseFloat(innerText.split('$').pop());
+  const totalPriceElement = document.getElementsByClassName('total-price')[0];
+  let curr = parseFloat(totalPriceElement.innerText);
+  curr -= price;
+  totalPriceElement.innerText = curr.toFixed(2);
   event.target.remove();
   updateStorage();
 }
@@ -27,10 +51,18 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
+const sumPrice = ({ price: salePrice }) => {
+  const totalPriceElement = document.getElementsByClassName('total-price')[0];
+  let curr = parseFloat(totalPriceElement.innerText);
+  curr += salePrice;
+  totalPriceElement.innerText = curr.toFixed(2); 
+};
+
 const fetchItems = async (id) => {
   const itemObj = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const itemJson = await itemObj.json();
   const item = createCartItemElement(itemJson);
+  sumPrice(itemJson);
   document.getElementsByClassName('cart__items')[0].appendChild(item);
   updateStorage();
 };
@@ -83,9 +115,12 @@ const cartLoad = () => {
   cartItems.forEach((element) => {
     element.addEventListener('click', cartItemClickListener);
   });
+  const totalPriceElement = document.getElementsByClassName('total-price')[0];
+  totalPriceElement.innerHTML = parseFloat(localStorage.totalPrice);
 };
 
 window.onload = function onload() {
+  totalPrice();
   fetchApi();
   cartLoad();
 };
