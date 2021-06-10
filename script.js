7b383fb9b1dd4d7d2c3d65c9c017a9f92f58fb10
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-09 22:45:17 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -18,23 +18,25 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) { 
   const section = document.createElement('section');
   section.className = 'item';
   
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   
+  const image = thumbnail.replace(/-I.jpg/g, '-O.jpg');
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  return event;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -42,14 +44,30 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+async function addItemsToCart(id) {
+  const API2 = `https://api.mercadolibre.com/items/${id}`;
+  const api = await fetch(API2);
+  const apiJSON = await api.json();
+  const cartItems = document.querySelector('.cart__items');
+  cartItems.appendChild(createCartItemElement(apiJSON));
+}
+
+const buttonEvents = () => {
+  const itemButton = document.querySelectorAll('.item__add');
+  itemButton.forEach((button) => button.addEventListener('click', ({ target }) => {
+    console.log(target.parentNode.firstChild.innerText);
+    addItemsToCart(target.parentNode.firstChild.innerText);
+  }));
+};
+
 const fetchAPI = async () => { 
   const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  let api = await fetch(API_URL); // pega, pelo API, os produtos consultados
-  let apiJSON = await api.json(); // transforma a promise em JSON
-  let arrayResult = apiJSON.results; // pega só o 'results' do retorno
-  await arrayResult.forEach((item) => items[0]
-  .appendChild(createProductItemElement(item)));
-}
+  const api = await fetch(API_URL); // pega, pelo API, os produtos consultados
+  const apiJSON = await api.json(); // transforma a promise em JSON
+  const arrayResult = apiJSON.results; // pega só o 'results' do retorno
+  await arrayResult.forEach((item) => items[0].appendChild(createProductItemElement(item)));
+  buttonEvents();
+};
 
 window.onload = function onload() {
   fetchAPI();
