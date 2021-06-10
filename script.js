4bd75bdf6cb5032ee1a7a612461734232965aa8e
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-09 21:34:02 -0300

    requisito 2 ainda sem pegar os nomes

--- script.js
+++ script.js
@@ -46,13 +46,29 @@ async function getProductsApi() {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const element = document.getElementsByClassName('cart__items');
+// console.log(element);
+async function getElementToCart(itemID) {
+  let elementChoice = await fetch(`https://api.mercadolibre.com/items/${itemID}`);
+  elementChoice = await elementChoice.json();
+  element[0].appendChild(createCartItemElement(itemID));
+  // console.log(elementChoice);
+}
+
+document.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('item__add')) {
+    const elementAdd = target.parentElement.firstChild.innerText;
+    getElementToCart(elementAdd);
+  }
+});
 
 window.onload = function onload() {
   getProductsApi();
