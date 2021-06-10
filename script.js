854064fce9f8d08f1151922cf3e32d560d59c424
Jossany Moura <mjossany@gmail.com>
2021-06-09 21:27:34 -0300

    Finaliza requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,30 +12,54 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function getSkuFromProductItem(item) {
+  //   return item.querySelector('span.item__sku').innerText;
+  // }
+  
+  // function cartItemClickListener(event) {
+    //   // coloque seu código aqui
+    // }
+    
+    // function createCartItemElement({ sku, name, salePrice }) {
+      //   const li = document.createElement('li');
+      //   li.className = 'cart__item';
+      //   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+      //   li.addEventListener('click', cartItemClickListener);
+      //   return li;
+      // }
+      
+      const capturaComputadores = () => new Promise((resolve, reject) => {
+          fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+          .then((response) => response.json())
+          .then((data) => resolve(data.results))
+          .catch((error) => reject(error));
+      });
+      
+      const fetchComputadores = async () => {
+        const catchItemsList = document.querySelector('.items');
+        try {
+          const computers = await capturaComputadores();
+          computers.forEach(({ id, title, thumbnail }) => {
+            const item = createProductItemElement({ id, title, thumbnail });
+            catchItemsList.appendChild(item);
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      
+      window.onload = function onload() {
+        fetchComputadores();
+      };
