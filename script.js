12a937ec24361e2beed399c7e9e8fa85bab1c973
Eder Santos <3derms@gmail.com>
2021-06-10 18:34:52 -0300

    Resolve o requisito 3

--- script.js
+++ script.js
@@ -28,17 +28,16 @@ function createProductItemElement({ sku, name, image }) {
 //  return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-  // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
-  console.log(sku, name, salePrice);
-const li = document.createElement('li');
-li.className = 'cart__item';
-li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-// li.addEventListener('click', cartItemClickListener);
-return li;
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
 }
 
 function clickItemAdd() {
@@ -54,7 +53,6 @@ function clickItemAdd() {
         product.sku = id;
         product.name = title;
         product.salePrice = price;
-        console.log(item);
         document.querySelector('.cart__items').appendChild(createCartItemElement(product));
       });
     });
