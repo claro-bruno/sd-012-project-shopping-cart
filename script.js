480e3caa7bf07562fdeec5298f83d53d5f4023bf
thales <thalesdaiel2013@gmail.com>
2021-06-10 20:24:34 -0300

    feito etapa 1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-window.onload = function onload() { };
-
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,24 +11,35 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
-
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement(elemento) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', elemento.id));
+  section.appendChild(createCustomElement('span', 'item__title', elemento.title));
+  section.appendChild(createProductImageElement(elemento.thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
+function objectValues(values) {
+  return Object.values(values);
+}
+async function returnFetch(search) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+    const json = await response.json();
+   const entries = objectValues(json.results);
+   entries.forEach((elemento) => {
+   
+   const section = document.querySelector('.items');
+  section.appendChild(createProductItemElement(elemento));
+  
+   });
+}
+
+window.onload = function onload() {
+  returnFetch('computador');
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
