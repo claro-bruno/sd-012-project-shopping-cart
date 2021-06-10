af64c8ace33d00dcc0607de0475f46ea2aace7e4
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-09 21:50:38 -0300

    requisito 3

--- script.js
+++ script.js
@@ -42,15 +42,13 @@ async function getProductsApi() {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+const cartItemClickListener = ({ target }) => target.remove();
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
