789abbb49aec22b19c97619ed6868ed9b7ca0ef2
Marcela Silva <marcela.ops@hotmail.br>
2021-06-09 21:23:12 -0300

    requiquito 2 ao meio

--- script.js
+++ script.js
@@ -32,22 +32,39 @@ const items = document.querySelector('.items');
 apiResults.forEach((item) => items.appendChild(createProductItemElement(item)));
 }
 
+// 2
+async function addProductToCart (id) {
+  const fetchItems = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const fetchJson = await fetchItems.json();
+  const cartItems = document.querySelector('.cart__items');
+  cartItems[0].appendChild(createCartItemElement(fetchJson));
+}
+
+function createCartItemElement({  id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function cartItemClickListener() {
+  // coloque seu código aqui event
+  document.addEventListener('click', function () {
+    const itemId = ;
+    
+  })
+}
+
+
+
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener() {
-//   // coloque seu código aqui event
-// }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
 
 window.onload = function onload() { 
 fetchApi();
+addProductToCart();
 };
