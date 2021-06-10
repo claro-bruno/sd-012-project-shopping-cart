4ddd685a25336487d50a5b0d559f090708b31f67
Renata Nunes <renatanunes231@yahoo.com.br>
2021-06-10 00:30:45 -0300

    Requisito 3 - Concluido

--- script.js
+++ script.js
@@ -48,15 +48,17 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener() {
-  return console.log('Funcao cartItemClickListener() nao definida ainda');
+function cartItemClickListener(event) {
+  event.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', (event) => {
+    cartItemClickListener(event.target);
+  });
   return li;
 }
 
