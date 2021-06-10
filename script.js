fadee428240fdc873cfb6592dc271e95c96a3d08
cristianocsl <cslcristiano@gmail.com>
2021-06-10 14:08:24 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createProductImageElement(imageSource) {
+function createProductImageElement(imageSource) { // cria elemento do tipo img
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
@@ -6,44 +6,50 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
+  const e = document.createElement(element); // que tipo de elemento é criado e quando?
+  e.className = className; // não entendi: acessa o elemento 'e' e define um className?
+  e.innerText = innerText; // não entendi: acessa o elemento 'e' e define um className?
   return e;
 }
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) { // define hierarquia etre elementos filhos de classe 'item' e elemento pai section de classe 'items'
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createProductImageElement(image.replace('I.jpg', 'O.jpg')));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
 
-// function getSkuFromProductItem(item) {
+// function getSkuFromProductItem(item) { // ???
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-function cartItemClickListener(event) {
-  console.log(event);
+function cartItemClickListener() { // o produto do carrinho é removido ao ser clicado. (remover class: projeto to-do-list)
+  const acessFather = document.querySelector('.cart__items');
+  const accessChild = document.querySelector('.cart__item');
+  acessFather.removeChild(accessChild);
 }
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) { // esses elementos devem estar na hierarquia de <ol class="cart__items"></ol>, no carrinho de compras.
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener); // ao clicar em um item com classe 'li', chama a função 'cartItemClickListener' que remove o 'li' do carrinho.
   return li;
 }
 
 const fetchObject = (id) => {
   fetch(`https://api.mercadolibre.com/items/${id}`)
+
   .then((response) => response.json())
-  .then((obj) => document.querySelector('.cart__items').appendChild(createCartItemElement(obj)));
+
+  .then((obj) => document.querySelector('.cart__items')
+
+  .appendChild(createCartItemElement(obj)));
 };
 
 const eventButtun = () => {
@@ -56,8 +62,11 @@ const eventButtun = () => {
 const fetchResults = () => {
   const getParent = document.getElementsByClassName('items');
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+
   .then((response) => response.json())
+
   .then((array) => array.results.forEach((item) => getParent[0]
+
   .appendChild(createProductItemElement(item)))).then(() => eventButtun());
 };
 
