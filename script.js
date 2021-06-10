35d6eb7180bc7c73c2336ad3794d7b30584c0293
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-10 19:22:39 -0300

    Implementa funçoes que cumprem requisitos 2 e 3

--- script.js
+++ script.js
@@ -32,6 +32,42 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
+function getItemInfo(itemID) {
+  return new Promise((resolve) => {
+    fetch(`https://api.mercadolibre.com/items/${itemID}`)
+    .then((response) => response.json())
+    .then((arr) => resolve(arr));
+  });
+}
+
+function removeCartItem(target) {
+  return document.querySelector('.cart__items').removeChild(target);
+}
+
+function cartItemClickListener(target) {
+  return target.addEventListener('click', () => removeCartItem(target));
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener(li));
+  return li;
+}
+
+function insertCartItem(event) {
+  const eventId = event.firstElementChild.innerText;
+  getItemInfo(eventId).then((itemInfo) => {
+    const newCartItemElement = createCartItemElement(itemInfo);
+    return document.querySelector('.cart__items').appendChild(newCartItemElement);
+  });
+}
+
+function addCartItemClickListener(target) {
+  return target.addEventListener('click', () => insertCartItem(target));
+}
+
 function insertItens(itemElement) {
   const itensSection = document.querySelector('.items');
   return itensSection.appendChild(itemElement);
@@ -41,22 +77,11 @@ window.onload = function onload() {
  getItensList()
   .then((itensInfo) => itensInfo.forEach((itemInfo) => {
     const newItemElement = createProductItemElement(itemInfo);
+    addCartItemClickListener(newItemElement);
     return insertItens(newItemElement);
   }));
 };
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener() {
-//   // coloque seu código aqui event
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
 // }
