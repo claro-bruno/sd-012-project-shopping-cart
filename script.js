2538b464b852068648efe3f8071fde535fa5d775
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-09 21:56:16 -0300

    caixa de mensagem para informar o usuario dos erros

--- script.js
+++ script.js
@@ -24,6 +24,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function clearMessage(event, container) {
+  container.removeChild(event.target.parentElement);
+}
+
+function message(text) {
+  const container = document.querySelector('.container');
+  const messageContainer = createCustomElement('div', 'message', '');
+  container.appendChild(messageContainer);
+  messageContainer.appendChild(createCustomElement('h2', 'message-title', 'Erro'));
+  messageContainer.appendChild(createCustomElement('p', 'text-message', text));
+  const btn = createCustomElement('button', 'btn-message', 'OK');
+  btn.addEventListener('click', (event) => clearMessage(event));
+  messageContainer.appendChild(btn);
+}
+
 function checkResponse(response) {
   if (response.ok) return response.json();
   throw new Error('Ocorreu um erro com a requisição');
@@ -87,7 +102,7 @@ function addItemInCart(event) {
       addInLocalStorage({ sku, name, salePrice });
       sumTotal();
     })
-    .catch((err) => console.log(err));
+    .catch(() => message('O item adicionado não está disponível'));
 }   
 
 function addBtnEvent() {
@@ -153,20 +168,5 @@ window.onload = function onload() {
 
   fetchApi('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then(({ results }) => addItems(results, items))
-    .catch((err) => console.log(err));
+    .catch(() => message('O produto procurado não foi encontrado'));
 };
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// };
