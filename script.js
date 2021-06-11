cac45445eae4da300030b89bba5861119a148fe0
camila-mp <ca.malvp@gmail.com>
2021-06-11 18:09:32 -0300

    resolve req 6

--- script.js
+++ script.js
@@ -18,16 +18,31 @@ const priceArray = [];
 
 function getTotalPrice({ salePrice }) {
   priceArray.push(salePrice);
-  const sumOfPrices = priceArray.reduce((acc, crr) => acc + crr, 0);
   const totalPrice = document.querySelector('.total-price');
+  const sumOfPrices = priceArray.reduce((acc, crr) => acc + crr, 0);
   totalPrice.innerText = `${sumOfPrices}`;
 }
 
 // ------------------------------------------------------------------------------
+
+function clearCart() {
+  const clearButton = document.querySelector('.empty-cart');
+  const olCart = document.querySelector(`.${cartItems}`);
+  clearButton.addEventListener('click', () => {
+    while(olCart.firstChild) {
+      olCart.removeChild(olCart.firstChild)
+    };
+    storeCart()
+  })
+}
+
+// ------------------------------------------------------------------------------
 const cartItems = 'cart__items';
+const sumOfPrices = 'total-price'
 
 function storeCart() {
   localStorage.setItem('shoppingCart', document.querySelector(`.${cartItems}`).innerHTML);
+  // localStorage.setItem('totalPriceCart', document.querySelector(`.${sumOfPrices}`).innerHTML);
 }
 
 // ------------------------------------------------------------------------------
@@ -68,8 +83,10 @@ function fetchProducts(url) {
 function cartItemClickListener(event) {
   event.target.remove();
   const priceToBeRemoved = Number(event.target.innerText.split('$')[1]);
-  const newObj = { salePrice: -priceToBeRemoved };
-  getTotalPrice(newObj);
+  if (priceToBeRemoved > 0) {
+    const newObj = { salePrice: -priceToBeRemoved };
+    getTotalPrice(newObj);
+  }
   storeCart();
 }
 // ------------------------------------------------------------------------------
@@ -112,6 +129,8 @@ function addEventAddToCart() {
 // ------------------------------------------------------------------------------
 
 function getStoredCart() {
+  // const storedPrice = localStorage.getItem('totalPriceCart')
+  // document.querySelector(`.${sumOfPrices}`).innerHTML = storedPrice;
   const storedCart = localStorage.getItem('shoppingCart');
   document.querySelector(`.${cartItems}`).innerHTML = storedCart;
   document.querySelectorAll('.cart__item').forEach((item) => 
@@ -124,4 +143,5 @@ window.onload = function onload() {
   fetchProducts('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   addEventAddToCart();
   getStoredCart();
+  clearCart()
 };
