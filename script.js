aa13f4b8f4bd19ecf5965643689fc9355c0323c8
MaxAlves0035 <maxalves2@hotmail.com>
2021-06-11 14:51:36 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +24,45 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+} */
+
+function showComputers(computer) {
+  const sectionItems = document.querySelector('.items');
+
+  const { id, title, thumbnail } = computer;
+
+  const createProduct = createProductItemElement({ id, title, thumbnail });
+
+  sectionItems.appendChild(createProduct);
 }
+
+function getApiComputers() {
+  return new Promise((resolve) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+      .then((response) => response.json())
+      .then((computers) => resolve(computers));    
+    });
+}
+function computersList() {
+   getApiComputers()
+    .then((computers) => {
+      computers.results.forEach((computer) => showComputers(computer));
+    });
+}
+window.onload = function onload() { 
+  computersList();
+};
