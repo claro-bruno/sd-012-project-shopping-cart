b5eaa50b9e987f95b61d8c305ab35723201d06c1
José Carlos <kalarruda@hotmail.com>
2021-06-11 16:30:52 -0300

    5-requisito somar valor

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const apiMercadoLivre = 'https://api.mercadolibre.com/sites/MLB/search?q=computa
 const listCart = document.querySelector('.cart__items');
 const ol = document.querySelector('.cart__items');
 const body = document.querySelector('body');
+const totalResult = document.querySelector('.total-price');
 
 // 5 requisito abaixo ----------------------------------------------------------
-
-const totalResult = document.querySelector('.total-price');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -50,7 +49,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
@@ -58,14 +57,36 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// 5 requisito abaixo ----------------------------------------------------------
+
+const currentPrice = () => {
+  totalResult.innerText = 0;
+};
+
+const totalPrices = () => {
+  const prices = listCart.childNodes; // pega todas as "li" da "ol"
+  console.log(prices);
+  const regExp = /\d*\.?\d*$/;
+  let result = 0;
+  prices.forEach((value) => { // esse descontrução pega 
+    result += parseFloat(value.innerHTML.match(regExp));
+  });
+  totalResult.innerHTML = result;
+};
+
+// 4 requisito abaixo e na função onload ---------------------------------------
+
+const saveLocalStorage = () => {
+  localStorage.setItem('myCart', listCart.innerHTML);
+};
+
 // 3 requisito abaixo----------------------------------------------------------
 
 function cartItemClickListener(event) {
   event.target.remove();
-  localStorage.removeItem(event.target);
+  saveLocalStorage();
+  totalPrices();
 }
-
-// -----------------------------------------------------------------------------
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
@@ -75,17 +96,14 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
-// 4 requisito abaixo e na função onload ---------------------------------------
-
-const saveLocalStorage = () => {
-  localStorage.setItem('myCart', listCart.innerHTML);
-};
-
 function loadLocalStorage() {
   listCart.innerHTML = localStorage.getItem('myCart');
   const teste = document.querySelectorAll('.cart__item');
   teste.forEach((li) => li.addEventListener('click', cartItemClickListener));
+  totalPrices();
 }
+
+currentPrice();
 
 // 2 requisito abaixo ----------------------------------------------------------
 
@@ -96,37 +114,28 @@ const addItens = async (id) => {
   // const ol = document.querySelector('.cart__items');
   ol.appendChild(createCartItemElement(idItem));
   saveLocalStorage();
+  totalPrices();
 };
 
 const addCart = () => {
   const btnAdd = document.querySelectorAll('.item__add');
 btnAdd.forEach((button) => {
   button.addEventListener('click', (event) => {
-    const getId = event.target.parentElement.firstChild.innerText; // para pegar o id
-    addItens(getId);
+    const getId = getSkuFromProductItem(event.target.parentNode); // para pegar o id    
+    addItens(getId);    
   });
 });
 };
 
-// 5 requisito abaixo ----------------------------------------------------------
-
-const currentPrice = () => {
-  totalResult.innerText = 0;
-};
-currentPrice();
-
-const totalPrices = () => {
-  const prices = listCart.childNodes; // pega todas as "li" da "ol"
-  console.log(prices);
-  const regExp = /\d*\.?\d*$/;
-  let result = 0;
-  prices.forEach((price) => {
-    result += parseFloat(price.innerHTML.match(regExp));
-  });
-  totalResult.innerHTML = result;
-  console.log('TESTE RESULT', result);
-};
-totalPrices();
+// const addCart = () => {
+//   const btnAdd = document.querySelectorAll('.item__add');
+// btnAdd.forEach((button) => {
+//   button.addEventListener('click', (event) => {
+//     const getId = event.target.parentElement.firstChild.innerText; // para pegar o id    
+//     addItens(getId);    
+//   });
+// });
+// };
 
 // 1 requisito abaixo ----------------------------------------------------------
 
@@ -138,7 +147,7 @@ const fetchML = async () => {
   const array = await apiJson.results;
   await array.forEach((item) => itens.appendChild(createProductItemElement(item)));
   addCart();
-  loadingDesapear();
+  loadingDesapear();  
 };
 
 // const fetchML = () => {
