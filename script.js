47dcf205868e421e54fa7e09a4ba9104999f04ef
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-06-11 15:50:28 -0300

    Resolve requisito 04

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Requisito 03 - Remove item do carrinho ao clicar
 function cartItemClickListener(event) {  
   // const remover = document.querySelector('.cart__items');
   // remover.removeChild(event.target);
   event.target.remove();
+  const removeElement = event.target.innerHTML.split('SKU: ')[1].split(' ')[0];
+  localStorage.removeItem(removeElement);
+  // console.log(event.target.innerHTML.split('SKU: ')[1].split(' ')[0]);
 }
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -25,15 +29,18 @@ function createCustomElement(element, className, innerText) {
   e.className = className;
   e.innerText = innerText;
   return e;
-}// Adicionando Produtos ao carrinho
+}
 const addProductCart = async (item) => {
   const idProduto = getSkuFromProductItem(item);
+  // Requisito 01 - Faz uma listagem dos produtos
   const add = document.querySelector('.cart__items');
-  
   const dadosProduto = await fetch(`https://api.mercadolibre.com/items/${idProduto}`);
   const result = await dadosProduto.json();
   const { id: sku, title: name, price: salePrice } = result;
-  add.appendChild(createCartItemElement({ sku, name, salePrice }));
+  const productStorage = createCartItemElement({ sku, name, salePrice });
+  add.appendChild(productStorage);
+  localStorage.setItem(sku, productStorage.innerHTML);
+  alert('Produto adicionado ao carrinho!');
 };
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -54,7 +61,7 @@ const computers = () => {
   method: 'GET',
   headers: { Authorization: 'Bearer $ACCESS_TOKEN' },
 };
-
+// Requisit 03 -Adiciona o item ao carrinho
 fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', obj) 
   .then((r) => r.json())
   .then((data) => data.results.forEach(({ id: sku, title: name, thumbnail: image }) =>
@@ -63,4 +70,14 @@ fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', obj)
 
 window.onload = function onload() {
 computers();
+console.log(localStorage.length);
+console.log(Object.keys(localStorage));
+  for (let index = 0; index < localStorage.length; index += 1) {
+  const text = Object.values(localStorage)[index];
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = text;
+  li.addEventListener('click', cartItemClickListener);
+  document.querySelector('.cart__items').appendChild(li);
+  }
 };
