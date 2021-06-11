9f9fd4070ac6271675b972e4525ebab153361e67
Willian Prado <willian_prado@hotmail.com>
2021-06-11 12:15:10 -0300

    Resolve o requisito 2

--- script.js
+++ script.js
@@ -24,17 +24,6 @@ function createProductItemElement({id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-async function getComputerAPI() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  const { results } = await response.json();
-  results.forEach((item) => {
-    const product = createProductItemElement(item);
-    console.log(product);
-    document.getElementsByClassName('items')[0].appendChild(product);
-  }); 
-} 
-
-/*
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -43,14 +32,43 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-*/
-window.onload = function onload() { 
-  getComputerAPI();
+
+async function fetchComputerAPI() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const { results } = await response.json();
+  results.forEach((item) => {
+    const product = createProductItemElement(item);
+    document.getElementsByClassName('items')[0].appendChild(product);
+  }); 
+} 
+
+async function fetchComputerByID(itemID) {
+  const url = `https://api.mercadolibre.com/items/${itemID}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const olElement = document.querySelector('.cart__items');
+  const liElement = createCartItemElement(data);
+  olElement.appendChild(liElement);
+}
+
+function addItemToCart() {
+  const items = document.querySelector('.items');
+  items.addEventListener('click', (event) => {
+    if (event.target.className === 'item__add') {
+      const itemID = getSkuFromProductItem(event.target.parentElement);
+      fetchComputerByID(itemID);
+    }
+  });
+}
+
+window.onload = async function onload() { 
+  await fetchComputerAPI();
+  addItemToCart();
 };
