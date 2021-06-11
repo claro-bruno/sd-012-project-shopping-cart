7b1c3e1d629d38d4bbd47e77d8cd2e621a1bb3eb
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-11 12:26:39 -0300

    ESLint

--- script.js
+++ script.js
@@ -2,55 +2,39 @@ const URL_PC = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 const URL_ITEM = 'https://api.mercadolibre.com/items/';
 let price;
 
-if (parseFloat(localStorage.getItem('total')) === NaN || localStorage.getItem('total') === null) {
+if (isNaN(parseFloat(localStorage.getItem('total'))) || localStorage.getItem('total') === null) {
   price = 0;
 } else {
   price = parseFloat(localStorage.getItem('total'));
 }
 
-const listPcs = (array) => {
-  const list = document.querySelector('.items');
-  array.forEach(element => {
-    list.appendChild(createProductItemElement(element));
-  });
+const createCustomElement = (element, className, innerText) => {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
 };
 
-const getApi = async (url) => {
-  try {
-    const response = await fetch(url);
-    const { results } = await response.json();
-    listPcs(results);
-    addClick(results);
-    loadStorage();
-    buttonRemove();
-    removeLoading();
-  } catch (error) {
-    alert(error);
-  }
+const createProductImageElement = (imageSource) => {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
 };
 
-const saveStorage = () => {
-  const array = [];
-  const ol = document.querySelectorAll('.cart__item');
-  for (let i = 0; i < ol.length; i += 1) {
-    array.push(ol[i].outerHTML);
-  }
-  localStorage.setItem('pcs', JSON.stringify(array));
+const createProductItemElement = ({ id, title, thumbnail }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
 };
 
-const loadStorage = () => {
-  const ol = document.querySelector('.cart__items');
-  const array = JSON.parse(localStorage.getItem('pcs'));
-  if (array !== null) {
-    for (let i = 0; i < array.length; i += 1) {
-      const li = document.createElement('li');
-      li.innerHTML = array[i];
-      li.firstChild.addEventListener('click', cartItemClickListener);
-      ol.appendChild(li.firstChild);
-    }
-  }
-  loadPrice();
-}
+const getSkuFromProductItem = (item) => {
+  return item.querySelector('span.item__sku').innerText;
+};
 
 const updatePrice = (total) => {
   const spanPrice = document.createElement('span');
@@ -82,50 +66,34 @@ const subPrice = (number) => {
   localStorage.setItem('total', price);
 };
 
-const createProductImageElement = (imageSource) => {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
+const saveStorage = () => {
+  const array = [];
+  const ol = document.querySelectorAll('.cart__item');
+  for (let i = 0; i < ol.length; i += 1) {
+    array.push(ol[i].outerHTML);
+  }
+  localStorage.setItem('pcs', JSON.stringify(array));
 };
 
-const createCustomElement = (element, className, innerText) => {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-};
-
-const createProductItemElement = ({ id, title, thumbnail }) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-  section.appendChild(createCustomElement('span', 'item__sku', id));
-  section.appendChild(createCustomElement('span', 'item__title', title));
-  section.appendChild(createProductImageElement(thumbnail));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  return section;
-};
-
-const getSkuFromProductItem = (item) => {
-  return item.querySelector('span.item__sku').innerText;
-};
-
-const cartItemClickListener = (event) => {
+const loadStorage = () => {
   const ol = document.querySelector('.cart__items');
-  ol.removeChild(event.target);
-  saveStorage();
-  const liText = event.target.innerText;
-  const indexIni = liText.indexOf('$');
-  const number = parseFloat(liText.slice(indexIni + 1));
-  subPrice(number);
-};
+  const array = JSON.parse(localStorage.getItem('pcs'));
+  if (array !== null) {
+    for (let i = 0; i < array.length; i += 1) {
+      const li = document.createElement('li');
+      li.innerHTML = array[i];
+      li.firstChild.addEventListener('click', cartItemClickListener);
+      ol.appendChild(li.firstChild);
+    }
+  }
+  loadPrice();
+}
 
-const createCartItemElement = ({ id, title, price }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+const listPcs = (array) => {
+  const list = document.querySelector('.items');
+  array.forEach((element) => {
+    list.appendChild(createProductItemElement(element));
+  });
 };
 
 const addPc = async (url) => {
@@ -179,6 +147,38 @@ const removeLoading = () => {
   const loading = document.querySelector('.loading');
   loading.remove();
 }
+
+const cartItemClickListener = (event) => {
+  const ol = document.querySelector('.cart__items');
+  ol.removeChild(event.target);
+  saveStorage();
+  const liText = event.target.innerText;
+  const indexIni = liText.indexOf('$');
+  const number = parseFloat(liText.slice(indexIni + 1));
+  subPrice(number);
+};
+
+const createCartItemElement = ({ id, title, price }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+const getApi = async (url) => {
+  try {
+    const response = await fetch(url);
+    const { results } = await response.json();
+    listPcs(results);
+    addClick(results);
+    loadStorage();
+    buttonRemove();
+    removeLoading();
+  } catch (error) {
+    alert(error);
+  }
+};
 
 window.onload = () => {
   getApi(URL_PC);
