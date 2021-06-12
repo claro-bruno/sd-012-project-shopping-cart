4f9801c68fea6c975f97a821d16aa2f365278773
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-12 17:45:33 -0300

    adicionado foto do item ao carrinho, finalizado projeto

--- script.js
+++ script.js
@@ -15,13 +15,11 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__details', 'ver detalhes'));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho! '));
-
   return section;
 }
 
@@ -63,22 +61,26 @@ function sumTotal() {
   const totalPrice = document.querySelector('.total-price');
   const cartItems = document.querySelectorAll('.cart__item');
   const priceItems = Array.prototype.map.call(cartItems, 
-      (item) => Number(item.innerHTML.slice(item.innerHTML.indexOf('$') + 1)))
+      (item) => Number(item.innerText.slice(item.innerText.indexOf('$') + 1)))
     .reduce((total, price) => total + price, 0);
   totalPrice.innerText = priceItems;
 }
 
 function cartItemClickListener(event) {
   const item = event.target;
+  if (item.src) item.parentNode.parentNode.removeChild(item.parentNode);
   item.parentElement.removeChild(item);
   removeItemStorage(item);
   sumTotal();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice, thumbnail }) { 
   const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.src = thumbnail;
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.appendChild(img);
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
@@ -94,12 +96,12 @@ function addItemInCart(event) {
   const cartItem = document.querySelector('.cart__items');
   const idItem = event.target.parentNode.firstChild.innerText;
   fetchApi(`https://api.mercadolibre.com/items/${idItem}`)
-    .then(({ id: sku, title: name, price: salePrice }) => {
+    .then(({ id: sku, title: name, price: salePrice, thumbnail }) => {
       const id = Date.now();
-      const li = createCartItemElement({ sku, name, salePrice });
+      const li = createCartItemElement({ sku, name, salePrice, thumbnail });
       li.id = id;
       cartItem.appendChild(li);
-      addInLocalStorage({ sku, name, salePrice, id });
+      addInLocalStorage({ sku, name, salePrice, thumbnail, id });
       sumTotal();
     })
     .catch(() => message('O item adicionado não está disponível'));
@@ -141,13 +143,13 @@ function createPicturesContainer(pictures, textDetails) {
 
 function createDetailsContainer({ title, pictures, price, warranty, id }) {
   const detailsContainer = createCustomElement('div', 'deatails-container', '');
+  detailsContainer.appendChild(createCustomElement('span', 'id-details', id));
   const btnBack = document.createElement('button');
-  btnBack.innerHTML = '<i class="fas fa-chevron-left"></i>';
+  btnBack.innerHTML = '<i class="fas fa-chevron-left"></i> Back';
   btnBack.addEventListener('click', (event) => returnToItems(event, detailsContainer));
   detailsContainer.appendChild(btnBack);
-  detailsContainer.appendChild(createCustomElement('span', 'id-details', id));
   detailsContainer.appendChild(createCustomElement('h2', 'details-title', title));
-  const textDetails = `Preço: ${price} \n Garantia: ${warranty}`;
+  const textDetails = `Preço: ${price.toFixed(2)} \n Garantia: ${warranty}`;
   detailsContainer.appendChild(createPicturesContainer(pictures, textDetails));
   const btn = createCustomElement('button', 'item__add', 'Adicionar ao carrinho! ');
   btn.addEventListener('click', addItemInCart);
@@ -226,16 +228,22 @@ function addLoading(items) {
   items.appendChild(loading);
 }
 
+function search(searchWord, items) {
+  const loading = items;
+  loading.innerHTML = '';
+  addLoading(items);
+  fetchApi(`https://api.mercadolibre.com/sites/MLB/search?q=${searchWord}`)
+    .then(({ results }) => addItems(results, items))
+    .catch(() => message('O produto procurado não foi encontrado'));
+} 
+
 window.onload = function onload() { 
   const items = document.querySelector('.items');
   const cartItems = document.querySelector('.cart__items');
-  
+  const searchWord = document.getElementById('search');
   createTotalPriceContainer();
   loadCart(cartItems);
   addEventEmptyCart(cartItems);
-  addLoading(items);
-
-  fetchApi('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(({ results }) => addItems(results, items))
-    .catch(() => message('O produto procurado não foi encontrado'));
+  search('computador', items);
+  searchWord.addEventListener('input', (event) => search(event.target.value, items));
 };
