e88b6e34f1e28802e3be9d41564ba98d372e43a7
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-06-12 02:05:44 -0300

    Alteraçoes e testes do req1 e req2 dia12.076

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const criarItensDaList = () => {
+  const array = [...document.querySelectorAll('.item__add')];
+  /* console.log(array); */
+  array.forEach((item) => {
+    //let sku = item.id
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+      .then((response) => response.json)
+        .then((response) => response);
+  }); 
+};
+// Cria as img's que abrigaram as imagens de cada um dos elementos html
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,6 +16,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// Cria as customizações de cada um dos elementos durante a criação deles em createProductItemElement().
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -12,6 +24,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Cria um elemento HTML, com característica especificas, para cada um dos itens retornados pela API em criaElementos().
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -24,33 +37,54 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
+// Requisito 1
+// Acessa todos os itens a partir do retorno do objeto da API
 const criaElementos = () => {
   const produtoAlvo = 'computador';
  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produtoAlvo}`)
  .then((response) => response.json())
- .then((response) => response.results.forEach((produto) => {
+ .then((response) => response.results.forEach((elemento) => {
    const elementoPai = document.querySelector('.items');
-   elementoPai.appendChild(createProductItemElement(produto));
- })); 
+   elementoPai.appendChild(createProductItemElement(elemento));
+ })).then(() => criarItensDaList()); 
 };
 
+// Captura o id de cada um dos itens e os submete ao método innerText
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// Requisito 2
+
+// Especifica as ações após o evento da addEventListener da createCartItemElement
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // coloque seu código aqui 
+  const itemClicado = event.target.id;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+// Cria o elemento da lista do carrinho de compras referente a cada item e submete tais elementos a um evento
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener());
   return li;
 }
-// Requisito 1
+createCartItemElement();
+/* const criarItensDaList = () => {
+  const array = [...document.querySelectorAll('.item__add')];
+  console.log(array);
+  array.forEach((item) => {
+    console.log(item);
+    let sku = item.
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+      .then((response) => response.json)
+        .then((response) => response);
+  }); 
+}; */
+
 window.onload = function onload() { 
   // Requisito 1
   criaElementos(); 
+  /* criarItensDaList(); */
 };
