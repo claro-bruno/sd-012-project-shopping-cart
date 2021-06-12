2c356cce59cb41d45827a51be4481db231ca41e0
martinsph <ph.mmartins@gmail.com>
2021-06-12 15:51:15 -0300

    add req 4

--- script.js
+++ script.js
@@ -12,14 +12,29 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+const nameKey = 'cartItem';
+const array = [];
+
+// Funcção para salvar itens no local storage
+const saveItem = (item) => {
+  console.log(item);
+  array.push(item);
+  console.log(array);
+  localStorage.setItem(nameKey, JSON.stringify(array));
+};
+
+// Função para remover item do carrinho
 function cartItemClickListener(e) {
   e.target.remove();
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+/* Função não utilizada
+function getSkuFromProductItem(item) {
+   return item.querySelector('span.item__sku').innerText;
+ }
+*/
 
+// Função para criar elementos no carrinho
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -29,16 +44,19 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
-const addToCart = (sku) => {
+// Função que encontra o Id na API
+const fetchId = (sku) => {
   const cart = document.querySelector('.cart__items');
   fetch(`https://api.mercadolibre.com/items/${sku}`)
     .then((response) => response.json())
     .then((response) => {
-      (cart.appendChild(createCartItemElement(response)));
+      cart.appendChild(createCartItemElement(response));
+      saveItem(response);
     })
     .catch((error) => (error));
 };
 
+// Função que cria lista de produtos
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -50,13 +68,24 @@ function createProductItemElement({ sku, name, image }) {
   const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
   // Para adicionar o evento na criação do botão tive ajuda do Emerson Filho!
   button.addEventListener('click', () => { 
-    addToCart(sku);
+    fetchId(sku);
   });
   section.appendChild(button);
 
   return section;
 }
 
+// Função que carrega os itens salvos, no carrinho
+const loadItems = () => {
+  const cartItem = localStorage.getItem(nameKey);
+  const cartItemString = JSON.parse(cartItem);
+  cartItemString.forEach((item) => {
+    const cart = document.querySelector('.cart__items');
+    cart.appendChild(createCartItemElement(item));
+  });
+};
+
+// Função que encontra a API do ML
 const getMLProducts = () => new Promise((resolve, reject) => {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
@@ -71,4 +100,6 @@ window.onload = function onload() {
        list.appendChild(createProductItemElement({ sku, name, image }));
     }))
     .catch((error) => error);
+
+  loadItems();
 };
