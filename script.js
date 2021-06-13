6de5dd85b4a26437b44b7a7d9599d03e60fe87a6
Elias Forte <eliasforte.dev@gmail.com>
2021-06-13 19:14:15 -0300

    entregue

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
+
   return section;
 }
 
