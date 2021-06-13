4fa73dec846545e5741ffaca55921a80c62d5f5b
Martin <escorpmartin97@gmail.com>
2021-06-13 00:50:15 -0300

    req 2

--- script.js
+++ script.js
@@ -28,29 +28,47 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+function fetchButton() {
+// const items = document.querySelectorAll('.item');
+ const buttons = document.querySelectorAll('.item__add');
+ buttons.forEach((button) => button.addEventListener('click', () => {
+  // const itemId = button.getSkuFromProductItem;
+   const id = button.parentNode.firstChild.innerText;
+   const url2 = `https://api.mercadolibre.com/items/${id}`;
+    fetch(url2)
+    .then((response) => response.json())
+    .then((objeto) => {
+      const ol = document.querySelector('.cart__items');
+      ol.appendChild(createCartItemElement(objeto));
+    });
+  }));
+}
+
 function fetchProduct() {
   fetch(url)
   .then((response) => response.json())
   .then((objeto) => objeto.results.forEach((element) => {
     itemsSection.appendChild(createProductItemElement(element));
-  }));
+  }))
+  .then(() => fetchButton());
 }
-// function getSkuFromProductItem(item) {
-  //   return item.querySelector('span.item__sku').innerText;
-  // }
-  
-  // function cartItemClickListener(event) {
-    //   // coloque seu código aqui
-    // }
-    
-    // function createCartItemElement({ sku, name, salePrice }) {
-      //   const li = document.createElement('li');
-      //   li.className = 'cart__item';
-      //   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-      //   li.addEventListener('click', cartItemClickListener);
-      //   return li;
-      // }
-      
-      window.onload = function onload() { 
-        fetchProduct();
-      };
+
+window.onload = function onload() { 
+  fetchProduct();
+};
