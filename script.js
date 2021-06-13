b0dd823a7c10f9caf294a48f6976d35ddbe03e3e
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-06-13 13:25:28 -0300

    Adiciona requisito 2 e 3.

--- script.js
+++ script.js
@@ -32,20 +32,20 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-const fetchItens = () => {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+const fetchItens = async () => {
+  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
     .then((list) => {
       const listComputer = list.results;
@@ -61,4 +61,31 @@ const fetchItens = () => {
     });
 };
 
-fetchItens();
+const addEventButton = (button, index) => {
+  const idListAdd = document.getElementsByClassName('item__sku');
+  const olCart = document.querySelector('.cart__items');
+  button.addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${idListAdd[index].innerText}`)
+      .then((response) => response.json())
+      .then((itemInfo) => {
+        const { id, title, price } = itemInfo;
+        const individualItem = {
+          sku: id,
+          name: title,
+          salePrice: price,
+        };
+        const liItem = createCartItemElement(individualItem);
+        olCart.appendChild(liItem);
+      });
+  });
+};
+
+const addItemToCart = async () => {
+  await fetchItens();
+  const buttonListAdd = document.getElementsByClassName('item__add');
+  for (let index = 0; index < buttonListAdd.length; index += 1) {
+    addEventButton(buttonListAdd[index], index);
+  }
+};
+
+addItemToCart();
