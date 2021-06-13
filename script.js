4291027acca5ae47eddff260f7188f1ecac8ec4c
Roberval Filho <rslfilho@gmail.com>
2021-06-13 10:57:02 -0300

    Refatorando algumas coisas e corrigindo quebra na exclusão de items

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const CART_ITEMS = '.cart__items';
 const LOCAL_STORAGE_KEY = 'shopCart';
 let itemStorage = [];
 
+const cartItemsIdUpdate = () => {
+  const cartItems = document.querySelectorAll('.cart__item');
+  cartItems.forEach((item, index) => {
+    const product = item;
+    product.id = `c${index}`;
+  });
+};
+
 const updateTotalPrice = () => {
   const totalPrice = itemStorage.reduce((acc, { price }) => acc + price, 0);
   const totalElement = document.querySelector('.total-price');
@@ -18,10 +26,10 @@ const addLocalStorage = (item) => {
 };
 
 const rmvLocalStorage = (cartItem) => {
-  const itemSKU = cartItem.innerText.substr(5, 13);
-  itemStorage = itemStorage.filter((item) => item.id !== itemSKU);
+  itemStorage = itemStorage.filter((item, index) => index !== Number(cartItem.id[1]));
   const itemStorageString = JSON.stringify(itemStorage);
   localStorage.setItem(LOCAL_STORAGE_KEY, itemStorageString);
+  cartItemsIdUpdate();
   updateTotalPrice();
 };
 
@@ -52,13 +60,14 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  const parentElement = document.querySelector(CART_ITEMS);
-  parentElement.removeChild(event.target);
+  event.target.remove();
   rmvLocalStorage(event.target);
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
+  const cartLength = document.querySelector('.cart__items').childNodes.length;
+  li.id = `c${cartLength}`;
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
