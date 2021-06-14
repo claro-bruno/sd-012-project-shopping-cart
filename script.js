d35964e2a6840e52fc657e66ac59d4f4f34753d2
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-06-14 16:29:08 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -12,11 +12,45 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id, title, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function getItem(id) {
+  const cart = document.querySelector('.cart__items');
+  try {
+    await fetch(`https://api.mercadolibre.com/items/${id}`).then((response) => {
+      response.json().then((product) => {
+        cart.appendChild(createCartItemElement(product));
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+ }
+
+//  const addButtonListeners = (() => {
+//   const items = document.querySelectorAll('.item');
+//   items.forEach((item) => {
+//     const newItemId = item.querySelector('.id').textContent;
+//     const newItemButton = item.querySelector('.item__add');
+//     newItemButton.addEventListener('click', getItem(newItemId));
+//   });
+// });
+
 function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'id', id));
   section.appendChild(createCustomElement('span', 'item__title', title));
   section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
@@ -28,36 +62,32 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 const getProducts = ((searchTerm) => new Promise((resolve, reject) => {
-    if (searchTerm === 'computador') {
-      const items = document.getElementsByClassName('items')[0];
-      fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchTerm}`).then((response) => {
-        response.json().then((products) => {
-          products.results.forEach((product) => {
-            items.appendChild(createProductItemElement(product));
-          });
+  if (searchTerm === 'computador') {
+    const items = document.querySelector('.items');
+    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchTerm}`).then((response) => {
+      response.json().then((products) => {
+        products.results.forEach((product) => {
+          items.appendChild(createProductItemElement(product))
+          .querySelector('.item__add').addEventListener('click', () => (getItem(product.id)));
+          // const newItemId = newItem.querySelector('.id').textContent;
+          // const newItemButton = newItem.querySelector('.item__add');
+          // newItemButton.addEventListener('click', getItem(newItemId));
         });
+        resolve(products.results);
       });
-    } else {
-      reject(new Error('Produto não aceito!'));
-    }
-  }));
+    });
+  } else {
+    return reject(new Error('Produto não aceito!'));
+  }
+}));
 
 const fetchProducts = async () => {
   try {
-    await getProducts('computador');
+    await getProducts('computador'); // .forEach((product) => {
+    //   document.querySelector('.items').appendChild(createProductItemElement(product)
+    //   .querySelector('.item__add').addEventListener('click', getItem(product.id)));
+    // });
   } catch (error) {
     console.log(error);
   }
@@ -65,4 +95,5 @@ const fetchProducts = async () => {
 
 window.onload = function onload() {
   fetchProducts();
+  // addButtonListeners();
 };
