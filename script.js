ff3732404a947bfd1e561d0ab2db5c4df836eff2
walttoncoelho <walltton@outlook.com>
2021-06-13 21:18:40 -0300

    Refatorando

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
 
   ol.appendChild(li); 
 
