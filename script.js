f219b9e40f1e7e1f6dbe41f21aed91dbde6b7c63
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-06-14 18:47:31 -0300

    adicionado comentários no código

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function cartItemClickListener(event) {
   // fazer depois
 }
 
+// requisito 2, vai criar um item de lista contendo as informações do objeto do produto selecionado.
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -10,11 +11,14 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
+// requisito 2, vai adicionar o item de lista a lista do carrinho de compras.
 function addItemCart(item) {
   const ol = document.querySelector('.cart__items');
   ol.appendChild(item);
 }
 
+// requisito 1 e 2, pega as informações da API e verifica se o resultado deu certo,
+// também verifica se a url passada é de um produto específico.
 const loadProductsAPI = async (url) => {
   try {
     const result = await fetch(url);
@@ -29,10 +33,14 @@ const loadProductsAPI = async (url) => {
   }
 };
 
+// requisito 2, é uma função que vai pegar o id do produto que foi clicado em
+// "adicionar ao carrinho" e vai retornar o objeto do produto selecionado.
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// requisito 2, é uma função que vai ser disparada com um click, se o alvo for um elemento que tenha
+// a classe "item_add", ele vai colocar o produto alvo e vai colocar no carrinho de compras.
 async function getProductId(event) {
   if (event.target.className === 'item__add') {
     const id = getSkuFromProductItem(event.target.parentElement);
@@ -41,6 +49,10 @@ async function getProductId(event) {
   }
 }
 
+// requisito 1, é responsável por criar os elementos com imagem que vão estar dentro da seção "item".
+// você deve ta se perguntando "mas porquê criar outra função só pra isso",
+// bom reclame com quem desenvolveu esses exercícios, eu fiz levando em consideração
+// que não podiamos alterar essas funções pré estabelecidas.
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -48,6 +60,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// requisito 1, é responsável por criar os elementos que vão estar dentro da seção "item".
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -55,6 +68,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// requisito 1, essa função é responsável por criar cada um dos produtos que foi solicitada a API com o termo "computador".
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -67,6 +81,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
+// requisito 1, adiciona os produtos (no caso computadores) no site.
 async function addProducts(url) {
   const sectionItems = document.querySelector('.items');
   const computers = await loadProductsAPI(url);
@@ -78,6 +93,7 @@ async function addProducts(url) {
   sectionItems.addEventListener('click', getProductId);
 }
 
+// vai ser executado depois que o html e css já estiver sido carregado.
 window.onload = () => {
   addProducts('https://api.mercadolibre.com/sites/MLB/search?q=computador');
 };
