020e5213a24ca03d2f8a97c215920aa44ecbcfb0
Gustavo-Mourao <gcmbh1@gmail.com>
2021-06-15 12:55:17 -0300

    Q3

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
