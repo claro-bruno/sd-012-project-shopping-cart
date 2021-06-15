91ac742a1df670505c99b689e89bc5aabab713d9
Ed Santos <edsantos2822@gmail.com>
2021-06-15 00:59:38 -0300

    Finalizando - Requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const takeSectionItems = document.querySelector('.items');
+const takeOlCartItem = document.querySelector('.cart__items');
 
+// APIs
+const PRODUCTS_API = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const PRODUCT_BY_ID = 'https://api.mercadolibre.com/items';
+
+// Cria elementos HTML para renderizar os produtos da API
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -10,7 +16,7 @@ function createProductImageElement(imageSource) {
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
-  e.innerText = innerText;
+  e.innerText = innerText; 
   return e;
 }
 
@@ -25,37 +31,57 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 
   return section;
 }
-/* 
+
+// Adiciona/remove produtos ao carrinho
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
-// Pega API do Mercado Livre e renderiza produtos na tela na tela
+// Pega API do Mercado Livre e renderiza produtos na tela na tela (Mentoria - Ronald)
 function takeProductsFromApi() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((response) => response.json())
-    .then((secondResponse) => secondResponse.results)
-    .then((products) => {
-      products.forEach((product) => {
-        // const { id: sku, title: name, thumbnail: image } = product;
-
-        takeSectionItems.appendChild(createProductItemElement(product));
+  return new Promise((resolve) => {
+    fetch(PRODUCTS_API)
+      .then((response) => response.json())
+      .then((secondResponse) => secondResponse.results)
+      .then((products) => {
+        products.forEach((product) => {
+          takeSectionItems.appendChild(createProductItemElement(product));
+        });
+        resolve();
       });
     });
 }
 
+// Adiciona evento aos botões dos produtos (Mentoria - Jensen)
+function takeProductButtons() {
+  const takeButtons = document.querySelectorAll('.item__add');
+  Array.from(takeButtons);
+  takeButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const sku = getSkuFromProductItem(event.target.parentNode);
+      
+      fetch(`${PRODUCT_BY_ID}/${sku}`)
+        .then((response) => response.json())
+        .then((secondResponse) => {
+          takeOlCartItem.appendChild(createCartItemElement(secondResponse));
+        });
+    });
+  });
+}
+
 window.onload = function onload() {
-  takeProductsFromApi();
+  takeProductsFromApi()
+    .then(() => takeProductButtons());
 };
