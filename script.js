fda50c9d9b5d1abe1eec881f53a5a28937613a0a
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-15 13:17:36 -0300

    resolve Requesito 3 mas nao resolve todo o  lint

--- script.js
+++ script.js
@@ -33,9 +33,11 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-
+// Requisito 3
+// Auxilio da Gisele no slack do Sérgio A. Barbosa
 function cartItemClickListener(event) {
-  return event;
+  const ol = document.querySelector('.cart__items');
+  event.target.remove(ol);
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -47,7 +49,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 }
 
 // Requesito 1
-// requesito 1, auxiliado por:https://www.youtube.com/watch?v=Zl_jF7umgcs&ab_channel=RogerMelo , aprendendo a usar async/await
+// Requisito 1, auxiliado por:https://www.youtube.com/watch?v=Zl_jF7umgcs&ab_channel=RogerMelo , aprendendo a usar async/await
 // E utilizando o Slack com a dúvida de Eder Santos
 const getProduct = async () => {
   const response = await fetch(urlComputador);
@@ -58,7 +60,7 @@ const getProduct = async () => {
   });
   addPurchases();
 };
-
+// Requisito 2, Auxiliado pelo slack de Diogo Sant'Anna
 const addPurchases = () => {
   const button = document.querySelectorAll('.item__add');
   const ol = document.querySelector('.cart__items');
