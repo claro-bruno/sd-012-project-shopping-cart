fd4e4e6a3cb607bf1d7dfd87f17794323b842f83
Victor Emmanuel <victorpassos2012@gmail.com>
2021-06-15 22:32:45 -0300

    requisito 3 feito

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  return event;
+  return event.target.remove();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
