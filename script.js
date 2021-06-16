702afac2cded6b4d4ea6380913ccf882ec3ceff4
Julio Pereira <jandrade.pereira1@gmail.com>
2021-06-16 20:27:59 -0300

    Requisito 2 e 3 - Insere respostas dos requisitos e modifica o nome da const addItemOnCart para getItemFromAPI

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const BASE_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
-const PRODUCT_ERROR_MESSAGE = 'Ops, o produto não foi encontrado :(';
+const BASE_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const PRODUCT_URL = 'https://api.mercadolibre.com/items/';
 const itemsSection = document.querySelector('.items');
+const cartsSection = document.querySelector('.cart__items');
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -16,47 +17,72 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
-
   section.className = 'item';
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  // itemsSection.appendChild(section);
-
   return section;
+};
+
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const cartItemClickListener = async (event) => {
+  // coloque seu código aqui
+  event.target.remove();
+};
+
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+const getItemFromAPI = async () => {
+  try {
+    const response = await fetch(BASE_URL);
+    const { results } = await response.json();
+    results.forEach((item) => {
+      const itemDetails = {
+        sku: item.id,
+        name: item.title,
+        image: item.thumbnail,
+      };
+      itemsSection.appendChild(createProductItemElement(itemDetails));
+    });
+  } catch (error) {
+    return error;
+  }
+};
+
+const selectItemToCart = () => {
+  itemsSection.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item__add')) {
+      const element = event.target.parentElement;
+      const sku = getSkuFromProductItem(element);
+      fetch(`${PRODUCT_URL}${sku}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const itemKey = {
+            sku,
+            name: data.title,
+            salePrice: data.price,
+          };
+          cartsSection.appendChild((createCartItemElement(itemKey)));
+        });
+    }
+  });
 };
 
 const productList = (object) => {
   object.forEach((element) => itemsSection.appendChild(createProductItemElement(element)));
-};
-
-const fetchProduct = async () => {
-  try {
-    const response = await fetch(BASE_URL);
-    const { results } = await response.json();
-    productList(results);
-  } catch (error) {
-    alert(PRODUCT_ERROR_MESSAGE);
-  }
-};
-
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-// const cartItemClickListener = (event) => {
-//   // coloque seu código aqui
-// };
-
-// const createCartItemElement = ({ id: sku, title: name, salePrice }) => {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// };
+}; 
 
 window.onload = function onload() {
-  fetchProduct();
-};
+  getItemFromAPI();
+  selectItemToCart();
+};  
