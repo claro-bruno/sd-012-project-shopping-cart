b3cef5ae8d0f0617564081e6adb2ab08ff3f3219
Camila Damásio <camiladamasioef@hotmail.com>
2021-06-16 12:22:09 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -12,10 +12,29 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// LocalStorage - setItem:
+function localStorageSetItems() {
+  // - Capturar a ol;
+  const ol = document.querySelector('ol');
+  // - Criar a função setItem para salvar o que é necessário; (Toda vez que houver um clique no add ao carrinho e toda vez que excluir algum produto ou esvaziar o carrinho)
+  localStorage.setItem('ol', ol.innerHTML);
+}
+
 function cartItemClickListener(event) {
   const cartItems = document.querySelector('.cart__items');
   cartItems.removeChild(event.target);
+  localStorageSetItems();
 }
+// LocalStorage - getItem:
+// - Criar a função de getItem para pegar o que foi salvo; (será executado na função que constrói a li);
+const localStorageGetItems = () => {
+  const ol = document.querySelector('.cart__items');
+  ol.innerHTML = localStorage.getItem('ol');
+  const cartItems = Object.values(document.getElementsByClassName('cart__item'));
+  cartItems.forEach((element) => {
+    element.addEventListener('click', cartItemClickListener);
+  });
+};
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
@@ -23,6 +42,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   document.querySelector('ol').appendChild(li);
+  localStorageSetItems();
 }
 
 const addCart = (sku) => {
@@ -73,6 +93,7 @@ const clearCart = () => {
     // } ESTA FOI A FORMA USADA PARA FAZER NO PROJETO TODOLIST, TAMBÉM FUNCIONA! PORÉM VOU DEIXAR UMA FORMA DIFERENTE.
   const allCartItems = document.querySelectorAll('.cart__item');
   allCartItems.forEach((product) => ol.removeChild(product));
+  localStorageSetItems();
   });
 };
 clearCart();
@@ -81,6 +102,9 @@ clearCart();
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-window.onload = async function onload() {
-  await fetchProducts();
+window.onload = function onload() {
+  fetchProducts();
+  localStorageGetItems();
 };
+
+// Falta refatorar e limpar o código, padronizar as funções utilizadas (algumas vezes foram utilizadas funções anônimas e outras arrow functions)
