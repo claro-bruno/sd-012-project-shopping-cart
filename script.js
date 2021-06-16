2caa9b94ae5d685b23c71f91bbe21ff744de136f
Sarah <sarahgsb@hotmail.com>
2021-06-15 23:30:26 -0300

    requisito 5 e lá vamos nós

--- script.js
+++ script.js
@@ -35,10 +35,36 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
+/* requisito 5
+ Consultei o resositório de Rafael Bamberg para resolver esse.
+ Link: https://github.com/tryber/sd-011-project-shopping-cart/pull/192
+
+/* A partir do valor inicial de 0, para cada item do array cart-item, o preço total somado
+é exibido na tela a partir do valor inicial de 0 atraves da iteração a cada item do array do carrinho
+como forEach, retornando o preço total em total-price.
+Foi preciso arredondar os valores e fixar em duas casas decimais para que os valores somados fiquem precisos no cálculo.
+Na linha 55 o uso de split para dividir a string onde encontram-se os dados de cada produto,
+foi utilizado ('$')[1] para que cada caractere correspondente a $ (o parametro do split) retorne somente os preços dos produtos,
+separando dos demais dados, sem isso o resultado da somatória total retorna NaN porque
+o codigo nao identifica o que tem que ser retornado.
+*/
+function totalPrice() {
+  const cartItem = document.querySelectorAll('.cart__item');
+  let count = 0;
+  cartItem.forEach((item) => {      
+    const value = parseFloat(item.innerHTML.split('$')[1]);
+    count = value + count;
+    return count.toFixed(2);
+  });
+  const total = document.querySelector('.total-price');
+  total.innerHTML = count;
+ }
+
 // requisito 3
 function cartItemClickListener(event) {
   const removeItem = event.target;
   removeItem.parentNode.removeChild(event.target);
+  totalPrice(); // requisito 5
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -55,12 +81,14 @@ async function fetchCartItems(ItemID) {
   const response = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
    const computer = await response.json();
    cartItems.appendChild(createCartItemElement(computer));
+   totalPrice(); // requisito 5
 }
 
 // requisito 6
 function removeCartItems() {
   emptyCart.addEventListener('click', () => {
     cart.innerHTML = '';
+    totalPrice(); // requisito 5
   });
 }
 
@@ -70,7 +98,8 @@ const addToCart = () => {
   addItem.forEach((button) => {
     button.addEventListener('click', (event) => {
       const item = event.target.parentElement.querySelector('.item__sku').innerText;
-      fetchCartItems(item).then((product) => createCartItemElement(product));    
+      fetchCartItems(item).then((product) => createCartItemElement(product));
+      totalPrice(); // requisito 5  
     });
   });
 };
@@ -92,5 +121,6 @@ async function fetchAPI() {
 window.onload = function onload() {
   fetchAPI()
   .then(() => addToCart())
-  .then(() => removeCartItems());
+  .then(() => removeCartItems())
+  .then(() => totalPrice());
 };
