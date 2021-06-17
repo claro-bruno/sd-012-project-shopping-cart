8e7d994051668ae249983bff8baa609f1d600108
Josimar Schneider <josimarschneider@gmail.com>
2021-06-16 22:02:40 -0400

    2 - Adicione o produto ao carrinho de compras

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const APIURL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const cartItems = document.querySelector('.cart__items');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -18,19 +19,33 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) { 
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+/* li.addEventListener('click', cartItemClickListener); */
   return li;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function addCartItem(ItemID) {
+  const productURL = `https://api.mercadolibre.com/items/${ItemID}`;
+  fetch(productURL)
+  .then((response) => response.json())
+  .then((response) => {
+    cartItems.appendChild(createCartItemElement(response));
+    localStorage.setItem('cartItem', cartItems.innerHTML);
+  });
+}
+
+function cartItemClickListener(event) {
+  if (event.target.className === ('item__add')) {
+    const tag = event.target.parentNode;
+    const itemSku = getSkuFromProductItem(tag);
+    addCartItem(itemSku);
+  }
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -39,19 +54,19 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  return section;
+  return section; 
 }
 
 function getItems() {
   const sectionItems = document.querySelector('.items');
   fetch(APIURL)
   .then((response) => response.json())
-  .then((response) => response.results)
-  .then((response) =>
-  response.forEach((current) => {
+  .then((response) => response.results.forEach((current) => {
   sectionItems.appendChild(createProductItemElement(current));
   }));
 }
+
 window.onload = function onload() {
   getItems();
+  document.addEventListener('click', cartItemClickListener);
 };
