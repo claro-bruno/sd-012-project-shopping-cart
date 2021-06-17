7a346d45b043499dbc13e8e468bbdc16589e3100
AndZoni <lorenzoniandre@outlook.com>
2021-06-17 14:29:54 +0100

    Add Requirement 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,30 +12,45 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+ } */
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const itens = document.querySelector('.items');
+  const promise = new Promise((resolve, reject) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => response.json().then((computer) => resolve(computer)));
+  });
+  promise.then((computer) => {
+    computer.results.forEach((item) => {
+      const { id: sku, title: name, thumbnail: image } = item;
+      const theItem = createProductItemElement({ sku, name, image });
+      itens.appendChild(theItem);
+    });
+  });
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
+
+window.onload = function onload() {
+  cartItemClickListener();
+ };
