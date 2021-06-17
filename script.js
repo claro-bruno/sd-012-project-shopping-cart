d74928be221f3eef332ec37321d619becde0756f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-06-17 15:00:51 -0300

    Concluindo requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const API_ML = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const URL_ITEM = 'https://api.mercadolibre.com/items/';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -32,7 +33,7 @@ function fetchProducts() {
     .then(({ results }) => results.forEach((infoProduct) => {
       listItens.appendChild(createProductItemElement(infoProduct));
     }));
-  }
+}
 /* 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -41,15 +42,33 @@ function getSkuFromProductItem(item) {
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
-
-function createCartItemElement({ sku, name, salePrice }) {
+*/
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
+  const list = document.querySelector('.cart__items');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-} */
+  list.appendChild(li);
+/*   li.addEventListener('click', cartItemClickListener);
+  return li; */
+}
+
+function fetchForId(id) {
+  fetch(`${URL_ITEM}${id}`)
+  .then((response) => response.json())
+  .then((results) => createCartItemElement(results));
+ }
+
+function listenEventAddToCart() {
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item__add')) {
+      const addressId = event.target.parentElement.firstElementChild.innerText;
+      fetchForId(addressId);
+    }
+  });
+}
 
 window.onload = function onload() {
   fetchProducts();
-};  
+  listenEventAddToCart();
+};
