34e00a38185f6c6691b318c054363e95520b255e
Lucas Basques <lukasbasques@gmail.com>
2021-06-17 21:34:15 -0300

    Requisito 1, corrige loop infinito

--- script.js
+++ script.js
@@ -14,30 +14,41 @@ function createCustomElement(element, className, innerText) {
 
 function createProductItemElement() {
   const urlProducts = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  const section = document.createElement('section');  
+  const section = document.createElement('section');
   section.className = 'item';
-  const idItem = document.querySelector('.items');
   fetch(urlProducts)
   .then((response) => response.json())
   .then((indice) => indice.results)
   .then((items) => {
-      (items.forEach((item) => { 
-      idItem.appendChild(createProductItemElement());
+      (items.forEach((item) => {
       section.appendChild(createCustomElement('span', 'item__sku', item.id));
       section.appendChild(createCustomElement('span', 'item__title', item.title));
       //  section.appendChild(createProductImageElement(item.image));
       section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   }));
   });
+
   return section;
 }
+
+const getSerchResults = () => {
+  const urlProducts = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const idItem = document.querySelector('.items');
+  fetch(urlProducts)
+  .then((response) => response.json())
+  .then((indice) => indice.results)
+  .then((items) => items.forEach((item) => idItem.appendChild(createProductItemElement())));
+};
+
+
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  addE
+
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -49,5 +60,5 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 window.onload = function onload() {
-  createProductItemElement();
+ getSerchResults();
 };
