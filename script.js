33fa79d033e2207ff9c27957d395054fdc0e0f70
Gutemberg <gttj2010@hotmail.com>
2021-06-18 10:50:27 -0300

    Requisito 1.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-window.onload = function onload() { };
+//window.onload = function onload() { };
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
+}
+
+function createProductItemElement({ id, title, thumbnail }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
 }
 
 function createCustomElement(element, className, innerText) {
