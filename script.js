f85e7f46c78dd61cd4efba06df571cc6df30d8c3
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-18 12:37:15 -0300

    resolve requesito 4 e começa o 5

--- script.js
+++ script.js
@@ -25,15 +25,17 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-  // item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 // Requisito 3
 // Auxilio da Gisele no slack do Sérgio A. Barbosa
 function cartItemClickListener(event) {
-  const ol = document.querySelector('.cart__items');
-  event.target.remove(ol);
+  const ol = document.querySelector('.cart__items')
+  event.target.remove(ol)
+  saveCart()
 }
+
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
@@ -46,18 +48,18 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 // Requisito 2, Auxiliado pelo slack de Diogo Sant'Anna
 const addPurchases = () => {
   const button = document.querySelectorAll('.item__add');
-  const ol = document.querySelector('.cart__items');
+  const ol = document.querySelector('.cart__items')
   button.forEach((b) => {
     b.addEventListener('click', () => {
       const id = b.parentNode.firstChild.innerText;
       const url = `https://api.mercadolibre.com/items/${id}`;
       fetch(url).then((response) => response.json().then((i) => {
-        ol.appendChild(createCartItemElement(i));
+        ol.appendChild(createCartItemElement(i))
+        saveCart()
       }));
     });
   });
 };
-
 // Requesito 1
 // Requisito 1, auxiliado por:https://www.youtube.com/watch?v=Zl_jF7umgcs&ab_channel=RogerMelo , aprendendo a usar async/await
 // E utilizando o Slack com a dúvida de Eder Santos
@@ -71,6 +73,29 @@ const getProduct = async () => {
   addPurchases();
 };
 
+// ffonte de como salvar o localStorage:
+// https://pt.stackoverflow.com/questions/329223/armazenar-um-array-de-objetos-em-um-local-storage-com-js#:~:text=Para%20salvar%20no%20localStorage%20%2C%20basta,parse%20.
+// https://www.youtube.com/watch?v=pLtAZF8FDXE&ab_channel=CFBCursos
+const saveCart = () => {
+  const ol = document.querySelector('.cart__items')
+  localStorage.setItem('cart', ol.innerHTML);
+};
+const addSaveCart = () => {
+  const ol = document.querySelector('.cart__items')
+  ol.innerHTML = localStorage.getItem('cart')
+  const list = document.querySelectorAll('.cart__items');
+  list.forEach((item) => item.addEventListener('click', cartItemClickListener))
+};
+const createPrice = () => {
+  const section = document.querySelector('.cart')
+  const priceTotal = document.createElement('p')
+  priceTotal.textContent = 'Total:'
+  priceTotal.className = 'total-price'
+  section.appendChild(priceTotal)
+};
+
 window.onload = function onload() { 
   getProduct();
+  addSaveCart();
+  createPrice();
 };
