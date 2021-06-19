5250cae69cfbd6c65e6a72f461faf7cc8e242a1a
EmmanuelBMS <acquabriall2020@gmail.com>
2021-06-19 17:08:49 -0300

    resolve req 3

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
