bc7a03b44ac2b287aea5c441edacb6739445a66c
Alexander <alexanderteodorocastro95@hotmail.com>
2021-07-02 15:49:43 -0300

    requisito 1 e 2

--- script.js
+++ script.js
@@ -15,31 +15,41 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-  
+
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
   return section;
 }
 
-/* function getSkuFromProductItem(item) {
+function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-} */
-
-/* function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
- */
 
-/* function createCartItemElement({ sku, name, salePrice }) {
+ function cartItemClickListener(event) {
+  console.log(event);
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+} 
+
+function click() {
+  const botoes = document.querySelectorAll('.item__add');
+  const lista = document.querySelector('.cart__items');
+  botoes.forEach((botao) => {
+    const IdName = getSkuFromProductItem(botao.parentNode);   
+    botao.addEventListener('click', () => fetch(`https://api.mercadolibre.com/items/${IdName}`)
+      .then((response) => response.json())
+      .then((product) => lista.appendChild(createCartItemElement(product)))
+      .then((xablau) => console.log(xablau)));
+  });
+}
 
 window.onload = function onload() {
   const sectionItem = document.querySelector('.items');
@@ -51,5 +61,6 @@ window.onload = function onload() {
       array.forEach((dados) => {
         sectionItem.appendChild(createProductItemElement(dados));
       });
-    });
-  };
+    })
+    .then(() => click());
+};
