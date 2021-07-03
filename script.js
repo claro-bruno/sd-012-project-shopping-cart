60ed4480d207fc071b859761a4debb08603dbb86
ViniciusMossi <vini_agg@hotmail.com>
2021-07-03 13:38:46 -0400

    acrescenta req 2 que add item ao carrinho

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -43,27 +43,55 @@ function createCartItemElement({ sku, name, salePrice }) {
 function generateItemsList() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then((response) => response.json())
-  .then((data) => {
+  .then((data) =>
     data.results.forEach((item) => {
-      createProductItemElement(item);
-      const section = document.querySelector('.items');
-      section.appendChild(createProductItemElement(item));
-    });
+      const objeto = {
+        sku: item.id,
+        name: item.title,
+        image: item.thumbnail,
+      };
+      // console.log(objeto);
+      // // o objeto está buscando os dados da api: a ID ( sku), o title e o thumbnail. Esta explicação está no plantão da T08 pelo slack.
+       document.querySelector('.items').appendChild(createProductItemElement(objeto));
+    }));
+}
+
+// function addItemToCart() {
+//   document.querySelector('.items').addEventListener('click', (event) => {
+//     if (event.target.classList.contains('item__add')) {
+//       const parentElement = event.target.parentElement;
+//       const sku = getSkuFromProductItem(parentElement);
+//       fetch(`https://api.mercadolibre.com/items/${sku}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const objeto = {
+//           sku,
+//           name: data.title,
+//           salePrice: data.price,
+//         };
+//         document.querySelector('.cart__items').appendChild(createCartItemElement(objeto));
+//       });
+//     }
+//   });
+// }
+
+function addItemToCart(id) {
+  const url = `https://api.mercadolibre.com/items/${id}`;
+  fetch(url)
+    .then((response) => {
+      response.json().then((itemList) => {
+        const section = document.getElementsByClassName('cart__items')[0];
+        section.appendChild(createCartItemElement(itemList));
+    }); 
   });
 }
 
-// function addItemToCart(id) {
-//   fetch(`https://api.mercadolibre.com/items/${id}`)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     data.results.forEach((element) => {
-//       createProductItemElement(element)
-//       shoppingCart.appendChild(createProductItemElement(element))
-//     })
-//   })
-// }
+document.addEventListener('click', (event) => {
+  if (event.target.className === 'item__add') {
+    addItemToCart(getSkuFromProductItem(event.target.parentElement));
+  }
+});
 
 window.onload = function onload() {
   generateItemsList();
-// addItemToCart();
 };
