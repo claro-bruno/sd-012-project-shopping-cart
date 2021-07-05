3e086d609bbdd16826d2113bf3038404c0375a83
Aldria Melo <aldriamelo.academico@gmail.com>
2021-07-05 12:31:03 -0300

    adiciona requisito 5

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // requisito 1 feito com suporte do colega Julio Barros
 const intensExibidos = document.querySelector('.items');
 const listaDeCompras = document.querySelector('.cart__items');
+const etiquetaDePreco = document.getElementById('etiqueta');
+
+// requisito 5 feito com suporte do colega Julio Barros
+function totalPrices() {
+  const listItems = document.querySelectorAll('.cart__item');
+  let sumOfPrices = 0;
+
+  listItems.forEach((item) => {
+    const valor = item.innerText;
+    const posicaoInicial = valor.indexOf('$') + 1;
+    const posicaoFinal = valor.length;
+    const stringTratada = valor.substr(posicaoInicial, posicaoFinal);
+    const numero = parseFloat(stringTratada);
+    sumOfPrices += numero;
+  });
+  etiquetaDePreco.innerText = sumOfPrices;
+}
 
 //  requisito 4
 function salvaStorage() {
   localStorage.setItem('salvaCarrinho', listaDeCompras.innerHTML);
+  localStorage.setItem('preco', etiquetaDePreco.innerHTML);
 }
 
-// requisito 1
+// requisito 1 feito com suporte do colega Julio Barros
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -25,11 +43,13 @@ function createProductImageElement(imageSource) {
 // requisito 3
 function cartItemClickListener(event) {
   event.target.remove();
+  totalPrices();
   salvaStorage();
 }
 // requisito 4
 function recuperaLista() {
   listaDeCompras.innerHTML = localStorage.getItem('salvaCarrinho');
+  etiquetaDePreco.innerHTML = localStorage.getItem('preco');
   const itensDoCarrinho = document.querySelectorAll('.cart__items');
   itensDoCarrinho.forEach((item) => item.addEventListener('click', cartItemClickListener));
 }
@@ -43,7 +63,7 @@ function createCartItemElement({ id, title, price }) {
   return li;
 }
 
-//  requisito 2
+//  requisito 2 feito com suporte do colega Julio Barros
 async function adicionaCarrinho(event) {
   const elementoPai = event.target.parentNode;
   const nomeProduto = elementoPai.firstChild.innerText;
@@ -53,13 +73,14 @@ async function adicionaCarrinho(event) {
     const result = await retorno.json();
     listaDeCompras.appendChild(createCartItemElement(result))
       .addEventListener('click', cartItemClickListener);
+      totalPrices();
       salvaStorage();
   } catch (error) {
     return error;
   }
 }
 
-// requisito 1
+// requisito 1 feito com suporte do colega Julio Barros
 function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -88,7 +109,6 @@ async function listadeProdutos() {
     return error;
   }
 }
-
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
